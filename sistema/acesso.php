4e64c7a3018d5cb0515c19ef21d549a6047496b5
Carlos Victor <lorovictor@yahoo.com.br>
2018-10-08 23:18:50 -0300

    Criação da tela de login

--- sistema/acesso.php
+++ sistema/acesso.php
@@ -2,19 +2,28 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:lamparinas.database.windows.net,1433; Database = DBLamparinas", "valmaregia", "cValToChar16");
+    $conn = new PDO("sqlsrv:server = tcp:lamparinas.database.windows.net,1433; 
+									 Database = DBLamparinas", "valmaregia", "cValToChar16");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
+    print("Erro de conexão com o banco.");
     die(print_r($e));
 }
 
-$sql = ("SELECT * FROM Usuario");
-$stmt = $conn->query("$sql");
+$psUsuario = $_POST['usuario'];
+$psSenha = md5($_POST['senha']);
 
-While ($row = $stmt->fetch()){
-	echo "$row[0] - $row[1]<br>";
+$usuario_escape = addslashes($psUsuario);
+$senha_escape = addslashes($psSenha);
+
+$sql = ("SELECT * FROM Usuario WHERE UsuarLogin = '" . $usuario_escape . "' and UsuarSenha = '".$senha_escape."'");
+
+echo $sql;
+$result = $conn->query("$sql");
+
+While ($row = $result->fetch()){
+	echo "$row[0] - $row[1] - $row[2] - $row[3]<br>";
 }
 $conn = NULL;
 
---
+++ sistema/login.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Lamparinas | Login</title>
+
+	<!-- Global stylesheets -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+	<link href="global_assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+	<link href="layout_1/LTR/default/full/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="layout_1/LTR/default/full/assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+	<link href="layout_1/LTR/default/full/assets/css/layout.min.css" rel="stylesheet" type="text/css">
+	<link href="layout_1/LTR/default/full/assets/css/components.min.css" rel="stylesheet" type="text/css">
+	<link href="layout_1/LTR/default/full/assets/css/colors.min.css" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script src="global_assets/js/main/jquery.min.js"></script>
+	<script src="global_assets/js/main/bootstrap.bundle.min.js"></script>
+	<script src="global_assets/js/plugins/loaders/blockui.min.js"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script src="layout_1/LTR/default/full/assets/js/app.js"></script>
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	<!-- Page content -->
+	<div class="page-content">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<!-- Content area -->
+			<div class="content d-flex justify-content-center align-items-center">
+
+				<!-- Login form -->
+				<form method="post" class="login-form" action="index.php">
+					<div class="card mb-0">
+						<div class="card-body">
+							<div class="text-center mb-3">
+								<!--<i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>-->
+								<img src="global_assets/images/lamparinas/logo-lamparinas.jpg" style="max-width: 200px;" />
+								<h5 class="mb-0">Acesse sua conta</h5>
+								<span class="d-block text-muted">Informe as credenciais abaixo</span>
+							</div>
+
+							<div class="form-group form-group-feedback form-group-feedback-left">
+								<input name="usuario" type="text" class="form-control" placeholder="Usuário...">
+								<div class="form-control-feedback">
+									<i class="icon-user text-muted"></i>
+								</div>
+							</div>
+
+							<div class="form-group form-group-feedback form-group-feedback-left">
+								<input name="senha" type="password" class="form-control" placeholder="Senha..." onKeyPress="if (event.keyCode == 13){document.forms[0].submit();document.forms[0].reset();}">
+								<div class="form-control-feedback">
+									<i class="icon-lock2 text-muted"></i>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary btn-block">Entrar <i class="icon-circle-right2 ml-2"></i></button>
+							</div>
+
+							<div class="text-center">
+								<a href="esqueceu-sua-senha.php">Esqueceu sua senha?</a>
+							</div>
+						</div>
+					</div>
+				</form>
+				<!-- /login form -->
+
+			</div>
+			<!-- /content area -->
+
+
+			<?php include_once("footer.php"); ?>
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+</body>
+</html>
