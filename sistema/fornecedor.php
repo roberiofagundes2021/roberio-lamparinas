af25ade7a639c93b61521295b180630737360ac8
Carlos Victor <lorovictor@yahoo.com.br>
2018-11-09 17:06:07 -0300

    Fornecedores Lista

---
+++ sistema/fornecedor.php
@@ -0,0 +1,173 @@
+<?php 
+
+include_once("sessao.php"); 
+
+$_SESSION['PaginaAtual'] = 'Fornecedor';
+
+include('global_assets/php/conexao.php');
+
+$sql = ("SELECT ForneId, ForneNomeFantasia, ForneRazaoSocial, ForneCnpj, ForneStatus
+		 FROM Fornecedor		 
+		 ORDER BY ForneNomeFantasia ASC");
+$result = $conn->query("$sql");
+$row = $result->fetchAll(PDO::FETCH_ASSOC);
+//$count = count($row);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Lamparinas | Perfil</title>
+
+	<?php include_once("head.php"); ?>
+	
+	<!-- Theme JS files -->
+	<script src="global_assets/js/plugins/tables/datatables/datatables.min.js"></script>
+	<script src="global_assets/js/plugins/tables/datatables/extensions/responsive.min.js"></script>
+	<script src="global_assets/js/plugins/forms/selects/select2.min.js"></script>
+
+	<script src="global_assets/js/demo_pages/datatables_responsive.js"></script>
+	<script src="global_assets/js/demo_pages/datatables_sorting.js"></script>
+	
+	<script src="global_assets/js/plugins/notifications/jgrowl.min.js"></script>
+	<script src="global_assets/js/plugins/notifications/noty.min.js"></script>
+	<script src="global_assets/js/demo_pages/extra_jgrowl_noty.js"></script>
+	<script src="global_assets/js/demo_pages/components_popups.js"></script
+	<!-- /theme JS files -->	
+	
+	<script>
+			
+		//Essa função foi criada para não usar $_GET e ficar mostrando os ids via URL
+		function atualizaFornecedor(ForneId, ForneNome, ForneStatus, Tipo){
+		
+			document.getElementById('inputFornecedorId').value = PerfilId;
+			document.getElementById('inputFornecedorNome').value = FornecedorNome;
+			document.getElementById('inputFornecedorStatus').value = FornecedorStatus;
+					
+			if (Tipo == 'edita'){	
+				document.formFornecedor.action = "fornecedorEdita.php";		
+			} else if (Tipo == 'exclui'){
+				confirmaExclusao(document.formFornecedor, "Tem certeza que deseja excluir esse fornecedor?", "fornecedorExclui.php");
+			} else if (Tipo == 'mudaStatus'){
+				document.formFornecedor.action = "fornecedorMudaSituacao.php";
+			}		
+			
+			document.formFornecedor.submit();
+		}		
+			
+	</script>
+
+</head>
+
+<body class="navbar-top">
+
+	<?php include_once("topo.php"); ?>	
+
+	<!-- Page content -->
+	<div class="page-content">
+		
+		<?php include_once("menu-left.php"); ?>
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<?php include_once("cabecalho.php"); ?>	
+
+			<!-- Content area -->
+			<div class="content">
+
+				<!-- Info blocks -->		
+				<div class="row">
+					<div class="col-lg-12">
+						<!-- Basic responsive configuration -->
+						<div class="card">
+							<div class="card-header header-elements-inline">
+								<h5 class="card-title">Relação de Fornecedores</h5>
+								<div class="header-elements">
+									<div class="list-icons">
+										<a class="list-icons-item" data-action="collapse"></a>
+										<a href="perfil.php" class="list-icons-item" data-action="reload"></a>
+										<!--<a class="list-icons-item" data-action="remove"></a>-->
+									</div>
+								</div>
+							</div>
+
+							<div class="card-body">
+								A relação abaixo faz referência aos fornecedores da empresa <b><?php echo $_SESSION['EmpreNomeFantasia']; ?></b>
+								<div class="text-right"><a href="fornecedorNovo.php" class="btn btn-success" role="button">Novo Fornecedor</a></div>
+							</div>
+							
+							<table class="table datatable-responsive">
+								<thead>
+									<tr class="bg-slate">
+										<th>Nome Fantasia</th>
+										<th>Razão Social</th>
+										<th>CNPJ</th>
+										<th>Situação</th>
+										<th class="text-center">Ações</th>
+									</tr>
+								</thead>
+								<tbody>
+								<?php
+									foreach ($row as $item){
+										
+										$situacao = $item['ForneStatus'] ? 'Ativo' : 'Inativo';
+										$situacaoClasse = $item['ForneStatus'] ? 'badge-success' : 'badge-secondary';
+										
+										print('
+										<tr>
+											<td>'.$item['ForneNomeFantasia'].'</td>
+											<td>'.$item['ForneRazaoSocial'].'</td>
+											<td>'.$item['ForneCnpj'].'</td>
+											');
+										
+										print('<td><a href="#" onclick="atualizaFornecedor('.$item['ForneId'].', \''.$item['ForneNomeFantasia'].'\','.$item['ForneStatus'].', \'mudaStatus\');"><span class="badge '.$situacaoClasse.'">'.$situacao.'</span></a></td>');
+										
+										print('<td class="text-center">
+												<div class="list-icons">
+													<div class="list-icons list-icons-extended">
+														<a href="#" onclick="atualizaFornecedor('.$item['ForneId'].', \''.$item['ForneNomeFantasia'].'\','.$item['ForneStatus'].', \'edita\');" class="list-icons-item"><i class="icon-pencil7"></i></a>
+														<a href="#" onclick="atualizaFornecedor('.$item['ForneId'].', \''.$item['ForneNomeFantasia'].'\','.$item['ForneStatus'].', \'exclui\');" class="list-icons-item"><i class="icon-bin"></i></a>
+													</div>
+												</div>
+											</td>
+										</tr>');
+									}
+								?>
+
+								</tbody>
+							</table>
+						</div>
+						<!-- /basic responsive configuration -->
+
+					</div>
+				</div>				
+				
+				<!-- /info blocks -->
+				
+				<form name="formPerfil" method="post">
+					<input type="hidden" id="inputFornecedorId" name="inputFornecedorId" >
+					<input type="hidden" id="inputFornecedorNome" name="inputFornecedorNome" >
+					<input type="hidden" id="inputFornecedorStatus" name="inputFornecedorStatus" >
+				</form>
+
+			</div>
+			<!-- /content area -->
+			
+			<?php include_once("footer.php"); ?>
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+	<?php include_once("alerta.php"); ?>
+
+</body>
+
+</html>
