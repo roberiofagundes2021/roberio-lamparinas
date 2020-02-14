e3a0027ca5def1f7cdf20ccf93f00ca23e133f0c
Carlos Victor <lorovictor@yahoo.com.br>
2020-02-14 00:02:27 -0300

    Relatório Estoque Mínimo

--- sistema/estoqueMinimoImprime.php
+++ sistema/estoqueMinimoImprime.php
@@ -9,12 +9,13 @@ use Mpdf\Mpdf;
 require_once 'global_assets/php/vendor/autoload.php';
 
 
-$sql = "SELECT DISTINCT ProduCodigo, ProduNome, ProduEstoqueMinimo, dbo.fnSaldoEstoque(MovimEmpresa, ProduId, MovimDestinoLocal) as saldo
+$sql = "SELECT DISTINCT ProduCategoria, CategNome, ProduCodigo, ProduNome, ProduEstoqueMinimo, dbo.fnSaldoEstoque(MovimEmpresa, ProduId, MovimDestinoLocal) as saldo
 	    FROM Produto
+	    JOIN Categoria on CategId = ProduCategoria
 	    JOIN MovimentacaoXProduto on MvXPrProduto = ProduId
 	    JOIN Movimentacao on MovimId = MvXPrMovimentacao
 	    WHERE MovimEmpresa = ".$_SESSION['EmpreId']." and dbo.fnSaldoEstoque(MovimEmpresa, ProduId, MovimDestinoLocal) < ProduEstoqueMinimo
-	    ORDER BY ProduCodigo
+	    Group By ProduCategoria, CategNome, ProduCodigo, ProduNome, ProduEstoqueMinimo, dbo.fnSaldoEstoque(MovimEmpresa, ProduId, MovimDestinoLocal)
 	    ";
 $result = $conn->query($sql);
 $rowProduto = $result->fetchAll(PDO::FETCH_ASSOC);
@@ -44,51 +45,73 @@ try {
 	 </div>
 	";		
 	
-	$html = '';
+	$html = '<div id="body" style="position: relative; margin: 100px 20px 50px 20px;">';
     
     $html .= '<br>
-                <div style="border-bottom: 1px solid #333; margin-bottom: -32px; margin-top: -50px">
+ 			  <div style="border-bottom: 1px solid #333; margin-bottom: -32px; margin-top: -50px">
                   <h3 style="text-align: center; padding-top: 40px; padding-bottom: -20px; margin-bottom:60px">Relação dos Produtos em estoque mínimo</h3>
 	          </div>
 	          <br>';
 
-	$html .= '
-				<br>
-				<table style="width:100%; border-collapse: collapse;">
-					<tr>
-						<th style="text-align: left; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:5%">Código</th>
-						<th style="text-align: left; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:38%">Descrição</th>
-						<th style="text-align: center; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:12%">Estoque Mínimo</th>
-						<th style="text-align: center; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:15%">Estoque atual</th>
-					</tr>
-				';	
-	    foreach ($rowProduto as $value) {
-		
-			$html .= "
-					<tr>
-						<td style='padding-top: 8px; padding-bottom: 8px; font-size: 11px;'>".$value['ProduCodigo']."</td>
-						<td style='padding-top: 8px; padding-bottom: 12px; font-size: 11px;'>".$value['ProduNome']."</td>
-						<td style='padding-top: 8px; text-align: center; padding-bottom: 12px; font-size: 11px;'>".$value['ProduEstoqueMinimo']."</td>
-						<td style='padding-top: 8px; text-align: center; padding-bottom: 12px; font-size: 11px;'>".$value['saldo']."</td>
-					</tr>
-					";
-	    }
-	    $html .= "</table>";
-				  
+
+	$categoriaVelha = 0;
+
+    foreach ($rowProduto as $item) {
+
+    	$categoriaNova = $item['ProduCategoria'];
+
+    	if ($categoriaNova <> $categoriaVelha){
+			
+			if($categoriaVelha <> 0){
+				$html .= "</table>";
+			}
+			
+			$html .= '<div style="position:relative; margin-top: 40px; text-transform: uppercase; font-weight: bold; background-color: #ccc; padding: 5px;">Categoria: '.$item['CategNome'].'</div>';
+					  
+			$html .= '
+			<br>
+			<table style="width:100%; border-collapse: collapse;">
+				<tr>
+					<th style="text-align: left; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:5%">Código</th>
+					<th style="text-align: left; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:38%">Descrição</th>
+					<th style="text-align: center; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:12%">Estoque Mínimo</th>
+					<th style="text-align: center; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:15%">Estoque atual</th>
+				</tr>
+			';							  
+		}
+	
+		$html .= "
+				<tr>
+					<td style='padding-top: 8px; padding-bottom: 8px; font-size: 11px;'>".$item['ProduCodigo']."</td>
+					<td style='padding-top: 8px; padding-bottom: 12px; font-size: 11px;'>".$item['ProduNome']."</td>
+					<td style='padding-top: 8px; text-align: center; padding-bottom: 12px; font-size: 11px;'>".$item['ProduEstoqueMinimo']."</td>
+					<td style='padding-top: 8px; text-align: center; padding-bottom: 12px; font-size: 11px;'>".$item['saldo']."</td>
+				</tr>
+				";
+
+		$categoriaVelha = $categoriaNova;
+    }
+
+    $html .= "</table>
+    		</div>
+    ";				  
 			
     
-    $rodape = "<hr/>
-    <div style='width:100%'>
+    $html .= "    
+    <div style='width:100%; border-top : 1px solid #0000FF; margin-top: 50px;'>
 		<div style='width:300px; float:left; display: inline;'>Sistema Lamparinas</div>
 		<div style='width:105px; float:right; display: inline;'>Página {PAGENO} / {nbpg}</div> 
-	</div>";
+	</div>
+
+	";
     
 $mpdf->SetHTMLHeader($topo);	
 	
    // $mpdf->SetHTMLHeader($topo,'O',true);
-    $mpdf->SetHTMLFooter($rodape);
+    //$mpdf->SetHTMLFooter($rodape);
     $mpdf->WriteHTML($html);
     
+
     // Other code
     $mpdf->Output();
     
