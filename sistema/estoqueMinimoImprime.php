9f1ca9d5601dbae4f404cb0ebf5889fd3de4cea4
Carlos Victor <lorovictor@yahoo.com.br>
2020-04-08 18:47:03 -0300

    Impressão Estoque Mínimo e Impressão da Movimentação

--- sistema/estoqueMinimoImprime.php
+++ sistema/estoqueMinimoImprime.php
@@ -21,17 +21,35 @@ $result = $conn->query($sql);
 $rowProduto = $result->fetchAll(PDO::FETCH_ASSOC);
 
 try {
-    $mpdf = new Mpdf([
-        'mode' => 'utf-8', 
-        //'format' => [190, 236], 
-        'format' => 'A4-P',
-        'default_font_size' => 10,
-		'default_font' => 'dejavusans',
-        //'orientation' => 'P', //P =>Portrait, L=> Landscape
-		'margin_top' => 30 // se quiser dar margin no header, aí seria 'margin_header'
-	]);
+	$mpdf = new mPDF([
+	             'mode' => 'utf-8',    // mode - default ''
+	             'format' => 'A4-P',    // format - A4, for example, default ''
+	             'default_font_size' => 9,     // font size - default 0
+	             'default_font' => '',    // default font family
+	             'margin-left' => 15,    // margin_left
+	             'margin-right' => 15,    // margin right
+	             'margin-top' => 158,     // margin top    -- aumentei aqui para que não ficasse em cima do header
+	             'margin-bottom' => 60,    // margin bottom
+	             'margin-header' => 6,     // margin header
+	             'margin-bottom' => 0,     // margin footer
+	             'orientation' => 'P']);  // L - landscape, P - portrait	
 	
-	$topo = "
+	$html = "
+
+	<style>
+		th{
+		    text-align: center; 
+		    border: #bbb solid 1px; 
+		    background-color: #f8f8f8; 
+		    padding: 8px;
+		}
+
+		td{
+			padding: 8px;				
+			border: #bbb solid 1px;
+		}
+	</style>
+	
 	<div style='position: relative; width:100%; border-bottom: 1px solid #000;'>
 		<div style='float:left; width: 400px; display: inline-block;'>
 			<img src='global_assets/images/lamparinas/logo-lamparinas_200x200.jpg' style='width:60px; height:60px; float:left; margin-right: 10px; margin-top:-10px;' />		
@@ -39,18 +57,13 @@ try {
 			<div style='position: absolute; font-size:10px; margin-top: 8px; margin-left:4px;'>Unidade: Secretaria de Saúde / Hospital Padre Manoel</div>
 		</div>
 		<div style='width:300px; float:right; display: inline-block; text-align:right; font-size: 10px; margin-top: -50px;'>
-			<div style='margin-bottom: 20px'>Data: {DATE j/m/Y}</div>
+			<div style='margin-bottom: 20px'>Data: ".date('d/m/Y')."</div>
 			<div style='margin-top:8px;font-weight:bold;'>Estoque Mínimo</div>
 		</div> 
-	 </div>
-	";		
-	    
-    $html = '
- 			  <div style="border-bottom: 1px solid #ccc; margin-bottom: -42px;">
-                  <h3 style="text-align: center; padding-top: 0px; padding-bottom: -20px; margin-bottom:40px">Relação dos Produtos em estoque mínimo</h3>
-	          </div>
-	          <br>';
-
+	</div>
+	 
+	<div style='text-align:center; margin-top: 20px;'><h1>RELAÇÃO DOS PRODUTOS EM ESTOQUE MÍNIMO</h1></div> 
+	";
 
 	$categoriaVelha = 0;
 
@@ -64,26 +77,26 @@ try {
 				$html .= "</table>";
 			}
 			
-			$html .= '<div style="position:relative; margin-top: 40px; text-transform: uppercase; font-weight: bold; background-color: #ccc; padding: 5px;">Categoria: '.$item['CategNome'].'</div>';
+			$html .= '<div style="position:relative; margin-top: 20px; text-transform: uppercase; font-weight: bold; background-color: #ccc; padding: 5px;">Categoria: '.$item['CategNome'].'</div>';
 					  
 			$html .= '
 			<br>
 			<table style="width:100%; border-collapse: collapse;">
 				<tr>
-					<th style="text-align: left; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:5%">Código</th>
-					<th style="text-align: left; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:38%">Descrição</th>
-					<th style="text-align: center; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:12%">Estoque Mínimo</th>
-					<th style="text-align: center; padding-top: 7px; padding-bottom: 7px; border-bottom: 1px solid #333; width:15%">Estoque atual</th>
+					<th style="text-align: center; width:10%">Código</th>
+					<th style="text-align: left; width:62%">Descrição</th>
+					<th style="text-align: center; width:18%">Estoque Mínimo</th>
+					<th style="text-align: center; width:15%">Estoque Atual</th>
 				</tr>
 			';							  
 		}
 	
 		$html .= "
 				<tr>
-					<td style='padding-top: 8px; padding-bottom: 8px; font-size: 11px;'>".$item['ProduCodigo']."</td>
-					<td style='padding-top: 8px; padding-bottom: 8px; font-size: 11px;'>".$item['ProduNome']."</td>
-					<td style='padding-top: 8px; text-align: center; padding-bottom: 8px; font-size: 11px;'>".$item['ProduEstoqueMinimo']."</td>
-					<td style='padding-top: 8px; text-align: center; padding-bottom: 8px; font-size: 11px;'>".$item['saldo']."</td>
+					<td style='text-align: center;'>".$item['ProduCodigo']."</td>
+					<td style='text-align: left;'>".$item['ProduNome']."</td>
+					<td style='text-align: center;'>".$item['ProduEstoqueMinimo']."</td>
+					<td style='text-align: center;'>".$item['saldo']."</td>
 				</tr>
 				";
 
@@ -98,9 +111,9 @@ try {
 		<div style='width:105px; float:right; display: inline;'>Página {PAGENO} / {nbpg}</div> 
 	</div>";
     
-	$mpdf->SetHTMLHeader($topo);	
+	//$mpdf->SetHTMLHeader($topo);	
     //$mpdf->SetHTMLHeader($topo,'O',true);
-    $mpdf->SetHTMLFooter($rodape);
+    $mpdf->SetHTMLFooter($rodape); //o SetHTMLFooter deve vir antes do WriteHTML para que o rodapé apareça em todas as páginas
     $mpdf->WriteHTML($html);    
 
     // Other code
@@ -109,5 +122,10 @@ try {
 } catch (\Mpdf\MpdfException $e) { // Note: safer fully qualified exception name used for catch
     
     // Process the exception, log, print etc.
-    echo $e->getMessage();
+    $html = $e->getMessage();
+	
+    $mpdf->WriteHTML($html);
+    
+    // Other code
+    $mpdf->Output();	
 }
