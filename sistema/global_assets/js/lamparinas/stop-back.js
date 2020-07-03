88bb0fbd420ee5a07a4c07d23fe64387dd5b95b6
Carlos Victor <lorovictor@yahoo.com.br>
2020-07-03 20:47:31 -0300

    stop-back.js

--- sistema/global_assets/js/lamparinas/stop-back.js
+++ sistema/global_assets/js/lamparinas/stop-back.js
@@ -1,3 +1,10 @@
+history.pushState(null, null, location.href); 
+history.back(); 
+history.forward(); 
+window.onpopstate = function () { 
+    history.go(1); 
+};
+/*
 history.pushState({
     page: 1
 }, "Categoria", "#");
@@ -5,4 +12,4 @@ window.addEventListener("popstate", function(event) {
     history.pushState({
         page: 1
     }, "Categoria", "#");
-});
+});*/
