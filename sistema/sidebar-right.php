97f64828f02d88106e8e949cd1e12232059f3f69
Carlos Victor <lorovictor@yahoo.com.br>
2020-10-27 23:02:56 -0300

    Resumo

--- sistema/sidebar-right.php
+++ sistema/sidebar-right.php
@@ -7,15 +7,15 @@
         <!-- Resumo do Financeiro -->
         <div class="card">
             
-            <div style="padding: 20px 10px 5px 10px; background: #ccc; text-align: center;">
+            <div style="padding: 30px 10px 15px 10px;background: #aaa;text-align: center;">
                 <h2 style="color:#333">Resumo do Financeiro</h2>
             </div>
 
-            <div style="padding: 10px 10px 0px 10px; background: #eee; text-align: center; border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;">
+            <div style="padding: 10px 10px 0px 10px;background: #EEEDED;text-align: center;border-top: 1px solid #ccc;border-bottom: 1px solid #ccc;">
                 <?php echo "<h3>".date("d/m/Y")."</h3>"; ?>
             </div>
 
-            <div style="padding: 10px 10px 0 10px;">
+            <div style="padding: 10px 10px 0 10px; background: #ccc;">
                 <div class="form-group" style="font-size:16px;">
                     <select id="cmbContaBanco" name="cmbContaBanco" class="form-control form-control-select2">
                         <option value="">Todos</option>
@@ -32,46 +32,46 @@
                             }
                         ?>
                     </select>
-                    <h3 class="form-text text-muted text-right">Conta</h3>
+                    <h3 class="form-text text-right" style="color: #666;">Conta</h3>
                 </div>
 
                 <div class="form-group">
                     <input id="inputCredito" name="inputCredito" class="form-control" value="1.400,00" style="font-size: 30px; text-align: right;">
-                    <h3 class="form-text text-muted text-right">Crédito</h3>
+                    <h3 class="form-text text-right" style="color: #666;">Crédito</h3>
                 </div> 
 
                 <div class="form-group">
                     <input id="inputDebito" name="inputDebito" class="form-control" value="1.200,00" style="font-size: 30px; text-align: right;">
-                    <h3 class="form-text text-muted text-right">Dédito</h3>
+                    <h3 class="form-text text-right" style="color: #666;">Débito</h3>
                 </div>                
 
                 <div class="form-group">
                     <input id="inputSaldo" name="inputSaldo" class="form-control" value="200,00" style="font-size: 30px; text-align: right;">
-                    <h3 class="form-text text-muted text-right"><b>Saldo</b> (Crédito - Débito)</h3>
+                    <h3 class="form-text text-right" style="color: #666;"><b>Saldo</b> (Crédito - Débito)</h3>
                 </div>
             </div>
         </div>
         <!-- /main navigation -->
 
-        <div style="background: #fff; height:10px; margin-top: 0px;"></div>
+        <div style="background: #ccc; height:10px; margin-top: 0px;"></div>
 
         <div class="card">
 
-            <div style="padding: 10px 10px 5px 10px; background: #eee; text-align: center; border-top: 1px solid #ccc; border-bottom: 1px solid #ccc;">
-                <?php echo "<h3>Acesso Rápido</h3>"; ?>
+            <div style="padding: 10px 10px 5px 10px;background: #aaa;text-align: center;border-top: 1px solid #ccc;border-bottom: 1px solid #ccc;">
+                <?php echo "<h2>Acesso Rápido</h2>"; ?>
             </div>        
             <div class="card-body" style="">
                 <div class="row">
                     <div class="col-12">
-                        <button type="button" class="btn btn-outline-primary legitRipple" style="width: 100%;">
+                        <button type="button" class="btn btn-outline bg-slate-600 text-slate-600 border-slate legitRipple" style="width: 100%;">
                             <span>PDV</span>
                         </button>
 
-                        <button type="button" class="btn btn-outline-primary legitRipple" style="width: 100%; margin-top: 10px;">
+                        <button type="button" class="btn btn-outline bg-slate-600 text-slate-600 border-slate legitRipple" style="width: 100%; margin-top: 10px;">
                             <span>Transferência</span>
                         </button>
 
-                        <button type="button" class="btn btn-outline-primary legitRipple" style="width: 100%; margin-top: 10px;">
+                        <button type="button" class="btn btn-outline bg-slate-600 text-slate-600 border-slate legitRipple" style="width: 100%; margin-top: 10px;">
                             <span>Movimentação</span>
                         </button>                        
                     </div>                    
