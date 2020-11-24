8ee3dc5d13554136feab5a7053e711d822e69230
unknown <fernandoarag@gmail.com>
2020-11-23 23:15:43 -0300

    Inicio da criação do contas a receber - FERNANDO - 23/11/2020

---
+++ sistema/contasAReceberFiltra.php
@@ -0,0 +1,173 @@
+<?php
+
+include_once("sessao.php");
+include('global_assets/php/conexao.php');
+
+function queryPesquisa()
+{
+    include('global_assets/php/conexao.php');
+
+    if($_POST['tipoFiltro'] == 'FiltroNormal')
+    {
+
+        $cont = 0;
+
+        $args = [];
+    
+        if (!empty($_POST['inputPeriodoDe']) || !empty($_POST['inputAte'])) {
+            empty($_POST['inputPeriodoDe']) ? $inputPeriodoDe = '1900-01-01' : $inputPeriodoDe = $_POST['inputPeriodoDe'];
+            empty($_POST['inputAte']) ? $inputAte = '2100-01-01' : $inputAte = $_POST['inputAte'];
+    
+            if($_POST['statusTipo'] == 'APAGAR'){
+                $args[]  = "CnAPaDtVencimento BETWEEN '" . $inputPeriodoDe . "' and '" . $inputAte . "' ";
+            } else {
+                $args[]  = "CnAPaDtPagamento BETWEEN '" . $inputPeriodoDe . "' and '" . $inputAte . "' ";                
+            }
+
+            if(!empty($_POST['inputPeriodoDe'])){
+                $_SESSION['ContPagPeriodoDe'] = $_POST['inputPeriodoDe'];
+            }
+
+            if(!empty($_POST['inputAte'])){
+                $_SESSION['ContPagAte'] = $_POST['inputAte'];
+            }
+        }
+    
+        if (!empty($_POST['cmbFornecedor'])) {
+            $args[]  = "CnAPaFornecedor = " . $_POST['cmbFornecedor'] . " ";
+            $_SESSION['ContPagFornecedor'] = $_POST['cmbFornecedor'];
+        }
+    
+        if (!empty($_POST['cmbPlanoContas'])) {
+            $args[]  = "CnAPaPlanoContas = " . $_POST['cmbPlanoContas'] . " ";
+            $_SESSION['ContPagPlanoContas'] = $_POST['cmbPlanoContas'];
+        }
+    
+        if (!empty($_POST['cmbStatus'])) {
+            $args[]  = "CnApaStatus = " . $_POST['cmbStatus'] . " ";
+            $_SESSION['ContPagStatus'] = $_POST['cmbStatus'];
+        }
+
+        if (count($args) >= 1) {
+
+            $string = implode(" and ", $args);
+    
+            if ($string != '') {
+                $string .= ' and ';
+            }
+    
+            $sql = "SELECT * 
+                    FROM ContasAPagar
+                    LEFT JOIN Fornecedor on ForneId = CnAPaFornecedor
+                    JOIN Situacao on SituaId = CnApaStatus
+                    WHERE " . $string . " CnAPaUnidade = " . $_SESSION['UnidadeId'] . "
+                ";
+            $result = $conn->query($sql);
+            $rowData = $result->fetchAll(PDO::FETCH_ASSOC);
+
+            count($rowData) >= 1 ? $cont = 1 : $cont = 0;
+        }
+    } else if(isset($_SESSION['ContPagPeriodoDe']) ||  isset($_SESSION['ContPagAte']) || isset($_SESSION['ContPagFornecedor']) || isset($_SESSION['ContPagPlanoContas']) || isset($_SESSION['ContPagStatus'])){
+
+        $cont = 0;
+        $args = [];
+        
+        if (!empty($_SESSION['ContPagPeriodoDe']) || !empty($_SESSION['ContPagAte'])) {
+            empty($_SESSION['ContPagPeriodoDe']) ? $inputPeriodoDe = '1900-01-01' : $inputPeriodoDe = $_SESSION['ContPagPeriodoDe'];
+            empty($_SESSION['ContPagAte']) ? $inputAte = '2100-01-01' : $inputAte = $_SESSION['ContPagAte'];
+    
+            $args[]  = "CnAPaDtVencimento BETWEEN '" . $inputPeriodoDe . "' and '" . $inputAte . "' ";
+        }
+    
+        if (!empty($_SESSION['ContPagFornecedor'])) {
+            $args[]  = "CnAPaFornecedor = " . $_SESSION['ContPagFornecedor'] . " ";
+        }
+    
+        if (!empty($_SESSION['ContPagPlanoContas'])) {
+            $args[]  = "CnAPaPlanoContas = " . $_SESSION['ContPagPlanoContas'] . " ";
+        }
+    
+        if (!empty($_SESSION['ContPagStatus'])) {
+            $args[]  = "CnApaStatus = " . $_SESSION['ContPagStatus'] . " ";
+        }
+
+        if (count($args) >= 1) {
+
+            $string = implode(" and ", $args);
+    
+            if ($string != '') {
+                $string .= ' and ';
+            }
+    
+            $sql = "SELECT * 
+                    FROM ContasAPagar
+                    LEFT JOIN Fornecedor on ForneId = CnAPaFornecedor
+                    JOIN Situacao on SituaId = CnApaStatus
+                    WHERE " . $string . " CnAPaUnidade = " . $_SESSION['UnidadeId'] . "
+                ";
+            $result = $conn->query($sql);
+            $rowData = $result->fetchAll(PDO::FETCH_ASSOC);
+
+            count($rowData) >= 1 ? $cont = 1 : $cont = 0;
+        }
+    } else {
+        $dataInicio = date("Y-m-d");
+        $dataFim = date("Y-m-d");
+
+        $sql = "SELECT * 
+                FROM ContasAPagar
+                LEFT JOIN Fornecedor on ForneId = CnAPaFornecedor
+                JOIN Situacao on SituaId = CnApaStatus
+                WHERE CnAPaUnidade = " . $_SESSION['UnidadeId'] . " and CnAPaDtVencimento BETWEEN '" . $dataInicio . "' and '" . $dataFim . "' and SituaChave = 'APAGAR'
+        ";
+        $result = $conn->query($sql);
+        $rowData = $result->fetchAll(PDO::FETCH_ASSOC);
+        count($rowData) >= 1 ? $cont = 1 : $cont = 0;
+    }
+
+    if ($cont == 1) {
+        $cont = 0;
+        print('<input type="hidden" id="elementosGrid" value="'.count($rowData).'">');
+        foreach ($rowData as $item) {
+            $cont++;     
+
+            $status = $item['CnAPaStatus'] == 11 ? 'À Pagar' : 'Paga';
+            $data = $_POST['statusTipo'] == 'APAGAR' ? mostraData($item['CnAPaDtVencimento']) : mostraData($item['CnAPaDtPagamento']);
+            print("
+            
+            <tr>
+                <td class='even'>
+                    <input type='checkbox' id='check".$cont."'>
+                    <input type='hidden' value='".$item['CnAPaId']."'>
+                </td>
+                <td class='even'><p class='m-0'>" . $data . "</p><input type='hidden' value='".$item['CnAPaDtVencimento']."'></td>
+                <td class='even'><a href='contasAPagarNovoLancamento.php?lancamentoId=".$item['CnAPaId']."'>" . $item['CnAPaDescricao'] . "</a></td>
+                <td class='even'>" . $item['ForneNome'] . "</td>
+                <td class='even' style='text-align: center'>" . $item['CnAPaNumDocumento'] . "</td>
+                <td class='even' style='text-align: center'>" . mostraValor($item['CnAPaValorAPagar']) . "</td>
+                <td class='even' style='text-align: center'>" .$status. "</td>
+                <td class='even d-flex flex-row justify-content-around align-content-center' style='text-align: center'>
+                <div class='list-icons'>
+                    <div class='list-icons list-icons-extended'>
+                        <a href='#' class='list-icons-item editarLancamento'  data-popup='tooltip' data-placement='bottom' title='Editar Conta'><i class='icon-pencil7'></i></a>
+                        <a href='#' idContaExcluir='".$item['CnAPaId']."' class='list-icons-item excluirConta'  data-popup='tooltip' data-placement='bottom' title='Excluir Conta'><i class='icon-bin'></i></a>
+				        <div class='dropdown'>													
+				        	<a href='#' class='list-icons-item' data-toggle='dropdown'>
+				        		<i class='icon-menu9'></i>
+				    
+				        	<div class='dropdown-menu dropdown-menu-right'>
+                                <a href='#' class='dropdown-item btnParcelar'  data-popup='tooltip' data-placement='bottom' title='Parcelar'><i class='icon-file-text2'></i> Parcelar</a>
+                                <a href='#' class='dropdown-item'  data-popup='tooltip' data-placement='bottom' title='Excluir Produto'><i class='icon-file-empty'></i></a>
+				        	</div>
+				        </div>
+				    </div>
+                   
+                    </div>
+                </td>
+            </tr>
+            ");
+        }
+    }
+}
+
+queryPesquisa();
---
+++ sistema/contasAReceberParcelamento.php
@@ -0,0 +1,137 @@
+<?php
+
+include_once("sessao.php");
+include('global_assets/php/conexao.php');
+
+if(isset($_POST['numParcelas'])){
+
+    $sql = "SELECT * 
+            FROM ContasAPagar
+            LEFT JOIN Fornecedor on ForneId = CnAPaFornecedor
+            WHERE CnAPaId = ".$_POST['idConta']." and CnAPaUnidade = " . $_SESSION['UnidadeId'] . "
+    ";
+    $result = $conn->query($sql);
+    $conta = $result->fetch(PDO::FETCH_ASSOC);
+
+    $sql = "SELECT SituaId
+	    FROM Situacao
+	    WHERE SituaChave = 'APAGAR'";
+    $result = $conn->query($sql);
+    $situacao = $result->fetch(PDO::FETCH_ASSOC);
+
+    $numParcelas = intVal($_POST['numParcelas']);
+    $parcelas = json_decode($_POST['parcelas'], true);
+
+    $numeroLinhasNaGrid = $_POST['elementosNaGride'];
+            
+    for($i = 0; $i <= $numParcelas - 1; $i++){
+    
+        $sql = "INSERT INTO ContasAPagar ( CnAPaPlanoContas, CnAPaFornecedor, CnAPaContaBanco, CnAPaFormaPagamento, CnAPaNumDocumento,
+                                      CnAPaNotaFiscal, CnAPaDtEmissao, CnAPaOrdemCompra, CnAPaDescricao, CnAPaDtVencimento, CnAPaValorAPagar,
+                                      CnAPaDtPagamento, CnAPaValorPago, CnAPaObservacao, CnAPaStatus, CnAPaUsuarioAtualizador, CnAPaUnidade)
+                VALUES ( :iPlanoContas, :iFornecedor, :iContaBanco, :iFormaPagamento,:sNumDocumento, :sNotaFiscal, :dateDtEmissao, :iOrdemCompra,
+                        :sDescricao, :dateDtVencimento, :fValorAPagar, :dateDtPagamento, :fValorPago, :sObservacao, :iStatus, :iUsuarioAtualizador, :iUnidade)";
+        $result = $conn->prepare($sql);
+
+        $result->execute(array(
+            
+                ':iPlanoContas' => $conta['CnAPaPlanoContas'],
+                ':iFornecedor' => $conta['CnAPaFornecedor'],
+                ':iContaBanco' => $conta['CnAPaContaBanco'],
+                ':iFormaPagamento' => $conta['CnAPaFormaPagamento'],
+                ':sNumDocumento' => $conta['CnAPaNumDocumento'],
+                ':sNotaFiscal' => $conta['CnAPaNotaFiscal'],
+                ':dateDtEmissao' => $conta['CnAPaDtEmissao'],
+                ':iOrdemCompra' => $conta['CnAPaOrdemCompra'],
+                ':sDescricao' => $parcelas[$i]['descricao'],
+                ':dateDtVencimento' => $conta['CnAPaDtVencimento'],
+                ':fValorAPagar' => floatval(str_replace(',', '.', $parcelas[$i]['valor'])),
+                ':dateDtPagamento' => $parcelas[$i]['vencimento'],
+                ':fValorPago' => null,
+                ':sObservacao' => $conta['CnAPaObservacao'],
+                ':iStatus' => $situacao['SituaId'],
+                ':iUsuarioAtualizador' => $_SESSION['UsuarId'],
+                ':iUnidade' => $_SESSION['UnidadeId']
+        ));
+
+        $parcelaId = $conn->lastInsertId();
+
+        $status = 'À Pagar';
+
+        $numeroLinhasNaGrid++;
+
+        print("
+        
+        <tr>
+            <td class='even'>
+                <input type='checkbox' id='check".$numeroLinhasNaGrid."'>
+                <input type='hidden' value='".$parcelaId."'>
+            </td>
+            <td class='even'><p class='m-0'>" . mostraData($parcelas[$i]['vencimento']) . "</p><input type='hidden' value='".$parcelas[$i]['vencimento']."'></td>
+            <td class='even'><a href='contasAPagarNovoLancamento.php?lancamentoId=".$parcelaId."'>" . $parcelas[$i]['descricao'] . "</a></td>
+            <td class='even'>" . $conta['ForneNome'] . "</td>
+            <td class='even' style='text-align: center'>" . $conta['CnAPaNumDocumento'] . "</td>
+            <td class='even' style='text-align: center'>" . $parcelas[$i]['valor'] . "</td>
+            <td class='even' style='text-align: center'>" .$status. "</td>
+            <td class='even d-flex flex-row justify-content-around align-content-center' style='text-align: center'>
+            <div class='list-icons'>
+                <div class='list-icons list-icons-extended'>
+                    <a href='#' class='list-icons-item editarLancamento'  data-popup='tooltip' data-placement='bottom' title='Excluir Produto'><i class='icon-pencil7'></i></a>
+                    <a href='#' idContaExcluir='".$parcelaId."' class='list-icons-item excluirConta'  data-popup='tooltip' data-placement='bottom' title='Excluir Conta'><i class='icon-bin'></i></a>
+                                    <div class='dropdown'>													
+                                            <a href='#' class='list-icons-item' data-toggle='dropdown'>
+                                                    <i class='icon-menu9'></i>
+                                
+                                            <div class='dropdown-menu dropdown-menu-right'>
+                            <a href='#' class='dropdown-item btnParcelar'  data-popup='tooltip' data-placement='bottom' title='Parcelar'><i class='icon-file-text2'></i> Parcelar</a>
+                            <a href='#' class='dropdown-item'  data-popup='tooltip' data-placement='bottom' title='Excluir Produto'><i class='icon-file-empty'></i></a>
+                                            </div>
+                                    </div>
+                                </div>
+                </div>
+            </td>
+        </tr>
+        ");
+
+
+        // print('<input type="hidden" id="elementosGrid" value="'.count($rowData).'">');
+
+        // $cont++;     
+        // $status = $item['CnAPaStatus'] == 11 ? 'À Pagar' : 'Paga';
+        // print("
+        
+        // <tr>
+        //     <td class='even'>
+        //         <input type='checkbox' id='check".$cont."'>
+        //         <input type='hidden' value='".$item['CnAPaId']."'>
+        //     </td>
+        //     <td class='even'><p class='m-0'>" . mostraData($item['CnAPaDtVencimento']) . "</p><input type='hidden' value='".$item['CnAPaDtVencimento']."'></td>
+        //     <td class='even'><a href='contasAPagarNovoLancamento.php?lancamentoId=".$item['CnAPaId']."'>" . $item['CnAPaDescricao'] . "</a></td>
+        //     <td class='even'>" . $item['ForneNome'] . "</td>
+        //     <td class='even' style='text-align: center'>" . $item['CnAPaNumDocumento'] . "</td>
+        //     <td class='even' style='text-align: center'>" . mostraValor($item['CnAPaValorAPagar']) . "</td>
+        //     <td class='even' style='text-align: center'>" .$status. "</td>
+        //     <td class='even d-flex flex-row justify-content-around align-content-center' style='text-align: center'>
+        //     <div class='list-icons'>
+        //         <div class='list-icons list-icons-extended'>
+        //             <a href='#' class='list-icons-item editarLancamento'  data-popup='tooltip' data-placement='bottom' title='Editar Conta'><i class='icon-pencil7'></i></a>
+        //             <a href='#' idContaExcluir='".$item['CnAPaId']."' class='list-icons-item excluirConta'  data-popup='tooltip' data-placement='bottom' title='Excluir Conta'><i class='icon-bin'></i></a>
+		// 	        <div class='dropdown'>													
+		// 	        	<a href='#' class='list-icons-item' data-toggle='dropdown'>
+		// 	        		<i class='icon-menu9'></i>
+			    
+		// 	        	<div class='dropdown-menu dropdown-menu-right'>
+        //                     <a href='#' class='dropdown-item btnParcelar'  data-popup='tooltip' data-placement='bottom' title='Parcelar'><i class='icon-file-text2'></i> Parcelar</a>
+        //                     <a href='#' class='dropdown-item'  data-popup='tooltip' data-placement='bottom' title='Excluir Produto'><i class='icon-file-empty'></i></a>
+		// 	        	</div>
+		// 	        </div>
+		// 	    </div>
+               
+        //         </div>
+        //     </td>
+        // </tr>
+        // ");
+    }
+}
+
+?>
