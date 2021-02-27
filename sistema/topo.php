71694549267df5f6c02d4bce7834d5cf3c1f558f
Carlos Victor <lorovictor@yahoo.com.br>
2021-02-27 10:07:51 -0300

    Troquei o icone do topo para compactar o menu left

--- sistema/topo.php
+++ sistema/topo.php
@@ -87,7 +87,7 @@
 		<ul class="navbar-nav">
 			<li class="nav-item">
 				<a href="#" class="navbar-nav-link sidebar-control sidebar-main-toggle d-none d-md-block">
-					<i class="icon-paragraph-justify3"></i>
+					<i class="icon-drag-left"></i>
 				</a>
 			</li>
 		</ul>
