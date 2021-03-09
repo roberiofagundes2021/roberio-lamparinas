04206c6afba06950127b6a17d538d77bc86b169a
Paulo <marques.psg@gmail.com>
2021-03-09 10:06:02 -0300

    Novo e Edita na mesma tela da Relação

--- sistema/modelo.php
+++ sistema/modelo.php
@@ -6,6 +6,7 @@ $_SESSION['PaginaAtual'] = 'Modelo';
 
 include('global_assets/php/conexao.php');
 
+//Essa consulta é para preencher a grid
 $sql = "SELECT ModelId, ModelNome, ModelStatus, SituaNome, SituaCor, SituaChave
 					FROM Modelo
 					JOIN Situacao on SituaId = ModelStatus
@@ -14,6 +15,71 @@ $sql = "SELECT ModelId, ModelNome, ModelStatus, SituaNome, SituaCor, SituaChave
 $result = $conn->query($sql);
 $row = $result->fetchAll(PDO::FETCH_ASSOC);
 //$count = count($row);
+
+//Se estiver editando
+if(isset($_POST['inputModeloId']) && $_POST['inputModeloId']){
+
+	//Essa consulta é para preencher o campo Nome com a modelo a ser editar
+	$sql = "SELECT ModelId, ModelNome
+			FROM Modelo
+			WHERE ModelId = " . $_POST['inputModeloId'];
+	$result = $conn->query($sql);
+	$rowModelo = $result->fetch(PDO::FETCH_ASSOC);
+		
+	$_SESSION['msg'] = array();
+} 
+
+//Se estiver gravando (inclusão ou edição)
+if (isset($_POST['inputEstadoAtual']) && substr($_POST['inputEstadoAtual'], 0, 5) == 'GRAVA'){
+
+	try{
+
+		//Edição
+		if (isset($_POST['inputEstadoAtual']) && $_POST['inputEstadoAtual'] == 'GRAVA_EDITA'){
+			
+			$sql = "UPDATE Modelo SET ModelNome = :sNome, ModelUsuarioAtualizador = :iUsuarioAtualizador
+					WHERE ModelId = :iModel";
+			$result = $conn->prepare($sql);
+					
+			$result->execute(array(
+							':sNome' => $_POST['inputNome'],
+							':iUsuarioAtualizador' => $_SESSION['UsuarId'],
+							':iModel' => $_POST['inputModeloId']
+							));
+	
+			$_SESSION['msg']['mensagem'] = "Modelo alterada!!!";
+	
+		} else { //inclusão
+		
+			$sql = "INSERT INTO Modelo (ModelNome, ModelStatus, ModelUsuarioAtualizador, ModelUnidade)
+					VALUES (:sNome, :bStatus, :iUsuarioAtualizador, :iUnidade)";
+			$result = $conn->prepare($sql);
+					
+			$result->execute(array(
+							':sNome' => $_POST['inputNome'],
+							':bStatus' => 1,
+							':iUsuarioAtualizador' => $_SESSION['UsuarId'],
+							':iUnidade' => $_SESSION['UnidadeId'],
+							));
+	
+			$_SESSION['msg']['mensagem'] = "Modelo incluída!!!";
+					
+		}
+	
+		$_SESSION['msg']['titulo'] = "Sucesso";
+		$_SESSION['msg']['tipo'] = "success";
+					
+	} catch(PDOException $e) {
+		
+		$_SESSION['msg']['titulo'] = "Erro";
+		$_SESSION['msg']['mensagem'] = "Erro reportado com a modelo!!!";
+		$_SESSION['msg']['tipo'] = "error";	
+		
+		echo 'Error: ' . $e->getMessage();
+	}
+
+	irpara("modelo.php");
+}
 
 ?>
 
@@ -39,7 +105,12 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 
 	<!-- Não permite que o usuário retorne para o EDITAR -->
 	<script src="global_assets/js/lamparinas/stop-back.js"></script>
-	<!-- /theme JS files -->
+	
+	<!-- Validação -->
+	<script src="global_assets/js/plugins/forms/validation/validate.min.js"></script>
+	<script src="global_assets/js/plugins/forms/validation/localization/messages_pt_BR.js"></script>
+	<script src="global_assets/js/demo_pages/form_validation.js"></script>	
+	
 
 	<script type="text/javascript">
 		$(document).ready(function() {
@@ -97,6 +168,42 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 			_componentSelect2();
 
 			/* Fim: Tabela Personalizada */
+
+			//Valida Registro Duplicado
+			$('#enviar').on('click', function(e){
+				
+				e.preventDefault();
+				
+				var inputNomeNovo = $('#inputNome').val();
+				var inputNomeVelho = $('#inputModeloNome').val();
+				var inputEstadoAtual = $('#inputEstadoAtual').val();
+				
+				//remove os espaços desnecessários antes e depois
+				inputNome = inputNomeNovo.trim();
+				
+				//Esse ajax está sendo usado para verificar no banco se o registro já existe
+				$.ajax({
+					type: "POST",
+					url: "modeloValida.php",
+					data: ('nomeNovo='+inputNome+'&nomeVelho='+inputNomeVelho+'&estadoAtual='+inputEstadoAtual),
+					success: function(resposta){
+
+						if(resposta == 1){
+							alerta('Atenção','Esse registro já existe!','error');
+							return false;
+						}
+
+						if (resposta == 'EDITA'){
+							document.getElementById('inputEstadoAtual').value = 'GRAVA_EDITA';
+						} else{
+							document.getElementById('inputEstadoAtual').value = 'GRAVA_NOVO';
+						}						
+						
+						$( "#formModelo" ).submit();
+					}
+				})
+			})				
+
 		});
 
 		//Essa função foi criada para não usar $_GET e ficar mostrando os ids via URL
@@ -107,7 +214,8 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 			document.getElementById('inputModeloStatus').value = ModelStatus;
 
 			if (Tipo == 'edita') {
-				document.formModelo.action = "modeloEdita.php";
+				document.getElementById('inputEstadoAtual').value = "EDITA";
+				document.formModelo.action = "modelo.php";		
 			} else if (Tipo == 'exclui') {
 				confirmaExclusao(document.formModelo, "Tem certeza que deseja excluir esse modelo?", "modeloExclui.php");
 			} else if (Tipo == 'mudaStatus') {
@@ -146,25 +254,45 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 						<!-- Basic responsive configuration -->
 						<div class="card">
 							<div class="card-header header-elements-inline">
-								<h3 class="card-title">Relação de Modelos</h3>
-								<div class="header-elements">
-									<div class="list-icons">
-										<a class="list-icons-item" data-action="collapse"></a>
-										<a href="modelo.php" class="list-icons-item" data-action="reload"></a>
-										<!--<a class="list-icons-item" data-action="remove"></a>-->
-									</div>
-								</div>
+								<h3 class="card-title">Relação de Modelos</h3>	
 							</div>
 
+
 							<div class="card-body">
-								<div class="row">
-									<div class="col-lg-9">
-										<p class="font-size-lg">A relação abaixo faz referência aos modelos da unidade <b><?php echo $_SESSION['UnidadeNome']; ?></b></p>
-									</div>	
-									<div class="col-lg-3">
-										<div class="text-right"><a href="modeloNovo.php" class="btn btn-principal" role="button">Novo Modelo</a></div>
+								
+								
+								<form name="formModelo" id="formModelo" method="post" class="form-validate-jquery">
+
+									<input type="hidden" id="inputModeloId" name="inputModeloId" value="<?php if (isset($_POST['inputModeloId'])) echo $_POST['inputModeloId']; ?>" >
+									<input type="hidden" id="inputModeloNome" name="inputModeloNome" value="<?php if (isset($_POST['inputModeloNome'])) echo $_POST['inputModeloNome']; ?>" >
+									<input type="hidden" id="inputModeloStatus" name="inputModeloStatus" >
+									<input type="hidden" id="inputEstadoAtual" name="inputEstadoAtual" value="<?php if (isset($_POST['inputEstadoAtual'])) echo $_POST['inputEstadoAtual']; ?>" >
+
+									<div class="row">
+										<div class="col-lg-6">
+											<div class="form-group">
+												<label for="inputNome">Nome da Modelo <span class="text-danger"> *</span></label>
+												<input type="text" id="inputNome" name="inputNome" class="form-control" placeholder="Modelo" value="<?php if (isset($_POST['inputModeloId'])) echo $rowModelo['ModelNome']; ?>" required autofocus>
+											</div>
+										</div>
+										<div class="col-lg-6">
+											<div class="form-group" style="padding-top:25px;">
+												<?php
+
+													//editando
+													if (isset($_POST['inputModeloId'])){
+														print('<button class="btn btn-lg btn-principal" id="enviar">Alterar</button>');
+														print('<a href="modelo.php" class="btn btn-basic" role="button">Cancelar</a>');
+													} else{ //inserindo
+														print('<button class="btn btn-lg btn-principal" id="enviar">Incluir</button>');
+													}
+
+												?>
+											</div>
+										</div>
 									</div>
-								</div>
+								</form>
+		
 							</div>
 
 							<table id="tblModelo" class="table">
@@ -210,12 +338,6 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 				</div>
 
 				<!-- /info blocks -->
-
-				<form name="formModelo" method="post">
-					<input type="hidden" id="inputModeloId" name="inputModeloId">
-					<input type="hidden" id="inputModeloNome" name="inputModeloNome">
-					<input type="hidden" id="inputModeloStatus" name="inputModeloStatus">
-				</form>
 
 			</div>
 			<!-- /content area -->
