844daa7db5fcdbbe18e5837e3904ca4672f88148
Paulo <marques.psg@gmail.com>
2021-03-17 00:25:17 -0300

    onde o Novo e o Edita está na mesma tela da relação.

--- sistema/usuarioLotacao.php
+++ sistema/usuarioLotacao.php
@@ -38,6 +38,39 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 //$count = count($row);
 //echo $sql;die;
 
+
+if(isset($_POST['cmbUnidade'])){
+		try{
+			//echo $_POST['cmbUnidade'];die;
+			$sql = "INSERT INTO UsuarioXUnidade (UsXUnEmpresaUsuarioPerfil, UsXUnUnidade, UsXUnSetor, UsXUnLocalEstoque, UsXUnUsuarioAtualizador)
+						VALUES (:iEmpresaUsarioPerfil, :iUnidade, :iSetor, :iLocalEstoque, :iUsuarioAtualizador)";
+			$result = $conn->prepare($sql);
+					
+			$result->execute(array(
+				':iEmpresaUsarioPerfil' => $_SESSION['EmpresaUsuarioPerfil'],
+				':iUnidade' => $_POST['cmbUnidade'],
+				':iSetor' => $_POST['cmbSetor'],
+				':iLocalEstoque' => isset($_POST['cmbLocalEstoque']) ? $_POST['cmbLocalEstoque'] : null,
+				':iUsuarioAtualizador' => $_SESSION['UsuarId']
+				));
+			
+			$_SESSION['msg']['titulo'] = "Sucesso";
+			$_SESSION['msg']['mensagem'] = "Lotação incluída!!!";
+			$_SESSION['msg']['tipo'] = "success";
+			
+		} catch(PDOException $e) {
+			
+			$_SESSION['msg']['titulo'] = "Erro";
+			$_SESSION['msg']['mensagem'] = "Erro ao incluir Lotação!!!";
+			$_SESSION['msg']['tipo'] = "error";	
+			
+			echo 'Error: ' . $e->getMessage();die;
+		}
+
+		irpara("usuarioLotacao.php");
+	}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -51,6 +84,11 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 	<?php include_once("head.php"); ?>
 	
 	<!-- Theme JS files -->
+
+
+	
+
+
 	<script src="global_assets/js/plugins/tables/datatables/datatables.min.js"></script>
 	<script src="global_assets/js/plugins/tables/datatables/extensions/responsive.min.js"></script>
 	
@@ -62,13 +100,92 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 
 	<script src="global_assets/js/demo_pages/datatables_responsive.js"></script>
 	<script src="global_assets/js/demo_pages/datatables_sorting.js"></script>
+
+	<script src="global_assets/js/plugins/forms/validation/validate.min.js"></script>
+  	<script src="global_assets/js/plugins/forms/validation/localization/messages_pt_BR.js"></script>
+  	<script src="global_assets/js/demo_pages/form_validation.js"></script>
 	
 	<!-- /theme JS files -->	
 	
 	<script type="text/javascript">
 		
 		$(document).ready(function() {
-			
+
+			//Valida Registro Duplicado
+			$('#enviar').on('click', function(e) {
+
+			e.preventDefault();
+
+				var cmbUnidade = $('#cmbUnidade').val(); 
+				var cmbSetor = $('#cmbSetor').val(); 
+					
+				if (cmbSetor == '') {
+				$("#formLotacao").submit();
+				} else {
+				//Esse ajax está sendo usado para verificar no banco se o registro já existe
+					$.ajax({
+						type: "POST",
+						url: "usuarioLotacaoValida.php",
+						data: ('unidade='+cmbUnidade),
+						success: function(resposta) {
+
+						if (resposta == 1) {
+							alerta('Atenção','Essa Unidade já existe!','error');
+							return false;
+						}
+
+						$("#formLotacao").submit();
+						}
+					})
+				}
+			})
+
+			$('#cmbUnidade').on('change', function(e) {
+
+				Filtrando();
+
+				var cmbUnidade = $('#cmbUnidade').val();
+
+				if (cmbUnidade == '') {
+				ResetSetor();
+				ResetLocalEstoque();
+				} else {
+
+				$.getJSON('filtraSetor.php?idUnidade=' + cmbUnidade, function(dados) {
+
+				var option = '<option value="">Selecione o Setor</option>';
+
+				if (dados.length) {
+
+					$.each(dados, function(i, obj) {
+					option += '<option value="' + obj.SetorId + '">' + obj.SetorNome + '</option>';
+					});
+
+					$('#cmbSetor').html(option).show();
+				} else {
+					ResetSetor();
+				}
+				});
+
+				$.getJSON('filtraLocalEstoque.php?idUnidade=' + cmbUnidade, function(dados) {
+
+					var option = '<option value="">Selecione o Local de Estoque</option>';
+
+						if (dados.length) {
+
+						$.each(dados, function(i, obj) {
+							option += '<option value="' + obj.LcEstId + '">' + obj.LcEstNome + '</option>';
+						});
+
+						$('#cmbLocalEstoque').html(option).show();
+						} else {
+							ResetLocalEstoque();
+						}
+					});
+
+				}
+			});
+						
 			/* Início: Tabela Personalizada */
 			$('#tblLotacao').DataTable( {
 				"order": [[ 0, "asc" ]],
@@ -136,6 +253,25 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 
 			document.formLotacao.submit();
 		}		
+
+		
+		
+
+		function Filtrando() {
+				$('#cmbSetor').empty().append('<option value="">Filtrando...</option>');
+				$('#cmbLocalEstoque').empty().append('<option value="">Filtrando...</option>');
+			}
+
+			function ResetSetor() {
+				$('#cmbSetor').empty().append('<option value="">Sem setor</option>');
+			}
+
+     	 function ResetLocalEstoque() {
+			$('#cmbLocalEstoque').empty().append('<option value="">Sem Local de Estoque</option>');
+		}	
+
+
+    	
 			
 	</script>
 
@@ -178,22 +314,77 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 						<!-- Basic responsive configuration -->
 						<div class="card">
 							<div class="card-header header-elements-inline">
-								<h3 class="card-title">Relação de Lotação</h3>
+								<h3 class="card-title">Relação de Lotação</h3>	
+								<div class="text-right">
+								 	<a href="usuario.php" style="margin-right: 10px;"><< Usuários</a>
+								</div>		
 							</div>
 
 							<div class="card-body">
-								<div class="row">
-									<div class="col-lg-9" class="card-body">	
-									A relação abaixo faz referência a lotação do usúario<span style="color: #FF0000; font-weight: bold;"> <?php echo $_SESSION['UsuarioNome']; ?></span> na empresa <b><?php echo $EmpresaNome; ?></b>	
-									</div>	
+								
+								<form name="formLotacao" id="formLotacao" method="post" class="form-validate-jquery" action="usuarioLotacaoNovo.php">
+								<input type="hidden" id="inputEmpresaUsuarioPerfil" name="inputEmpresaUsuarioPerfil" >
+								<input type="hidden" id="inputUnidade" name="inputUnidade" >
+									<div class="card-body">
+										<div class="row">
+											<div class="col-lg-3">
+												<div class="form-group">
+													<label for="cmbUnidade">Unidade<span class="text-danger"> *</span></label>
+													<select name="cmbUnidade" id="cmbUnidade" class="form-control form-control-select2" required>
+													<option value="">Informe uma unidade</option>
+													<?php
+													$sql = "SELECT UnidaId, UnidaNome
+															FROM Unidade
+															JOIN Situacao on SituaId = UnidaStatus															     
+															WHERE UnidaEmpresa = " . $EmpresaId . " and SituaChave = 'ATIVO'
+															ORDER BY UnidaNome ASC";
+													$result = $conn->query($sql);
+													$rowUnidade = $result->fetchAll(PDO::FETCH_ASSOC);
+
+													foreach ($rowUnidade as $item) {
+														print('<option value="' . $item['UnidaId'] . '">' . $item['UnidaNome'] . '</option>');
+													}
+
+													?>
+													</select>
+												</div>
+											</div>
+
+											<div class="col-lg-3">
+												<div class="form-group">
+													<label for="cmbSetor">Setor<span class="text-danger"> *</span></label>
+													<select name="cmbSetor" id="cmbSetor" class="form-control form-control-select2" required>
+													<option value="">Sem setor</option>
+													</select>
+												</div>
+											</div>	
+											
+											<?php 
+
+											if ($_SESSION['UsuarioPerfil'] == 'ALMOXARIFADO'){
+												print('
+												<div class="col-lg-3">
+													<div class="form-group">
+													<label for="cmbLocalEstoque">Local de Estoque<span class="text-danger"> *</span></label>
+													<select name="cmbLocalEstoque" id="cmbLocalEstoque" class="form-control form-control-select2" required>
+														<option value="">Local de Estoque</option>
+													</select>
+													</div>
+												</div>
+												');
+											}
+
+											?>
 									
-									<div class="col-lg-3">	
-										<div class="text-right">
-											<a href="usuario.php" style="margin-right: 10px;"><< Usuários</a>
-											<a href="usuarioLotacaoNovo.php" class="btn btn-principal" role="button">Nova Lotação</a>
+											<div class="col-lg-3" style="margin-top: 20px;">
+												<div class="form-group">
+													<button class="btn btn-lg btn-principal" id="enviar">Incluir</button>
+													<a href="usuarioLotacao.php" class="btn btn-basic" role="button">Cancelar</a>
+												</div>
+											</div>
 										</div>
-									</div>
-								</div>
+									</div>	
+								</form>			
 							</div>
 							
 							<table class="table" id="tblLotacao">
@@ -240,10 +431,6 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 				
 				<!-- /info blocks -->
 				
-				<form name="formLotacao" method="post">
-					<input type="hidden" id="inputEmpresaUsuarioPerfil" name="inputEmpresaUsuarioPerfil" >
-					<input type="hidden" id="inputUnidade" name="inputUnidade" >
-				</form>
 
 			</div>
 			<!-- /content area -->
