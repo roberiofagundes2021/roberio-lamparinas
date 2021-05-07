c400d6b5064611fd2d4b85762c4c04be04feaeaa
Carlos Victor <lorovictor@yahoo.com.br>
2021-05-06 22:06:18 -0300

    Removido código desnecessário

--- sistema/contrato.php
+++ sistema/contrato.php
@@ -194,11 +194,11 @@ $row = $result->fetchAll(PDO::FETCH_ASSOC);
 							<div class="card-header header-elements-inline">
 								<h3 class="card-title">Relação dos Contratos</h3>
 								<div class="header-elements">
-									<div class="list-icons">
+									<!--<div class="list-icons">
 										<a class="list-icons-item" data-action="collapse"></a>
 										<a href="contrato.php" class="list-icons-item" data-action="reload"></a>
-										<!--<a class="list-icons-item" data-action="remove"></a>-->
-									</div>
+										<a class="list-icons-item" data-action="remove"></a>
+									</div>-->
 								</div>
 							</div>
 
