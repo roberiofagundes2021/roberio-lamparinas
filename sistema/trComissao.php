224a9a39012d6fea97858b745bfd3c5bef57a131
Paulo <marques.psg@gmail.com>
2021-05-24 20:44:15 -0300

    anexo tr comissao processo licitatorio

--- sistema/trComissao.php
+++ sistema/trComissao.php
@@ -6,10 +6,24 @@ $_SESSION['PaginaAtual'] = 'Comissão do Processo Licitatório';
 
 include('global_assets/php/conexao.php');
 
+	
+
 if (isset($_POST['inputTRId'])){
 	$_SESSION['TRId'] 		= $_POST['inputTRId'];
 	$_SESSION['TRNumero'] = $_POST['inputTRNumero'];
-}
+
+
+} 
+
+$sql = "
+			SELECT TRXCoId, TRXCoData, TRXCoNome, TRXCoArquivo
+			FROM TRXComissao
+			WHERE TRXCoUnidade = ". $_SESSION['UnidadeId'] ." AND TRXCoTermoReferencia = ".$_SESSION['TRId']."	
+			ORDER BY TRXCoNome ASC	
+		";
+		$result = $conn->query($sql);
+		$rowAnexo = $result->fetchAll(PDO::FETCH_ASSOC);
+		//$count = count($row);
 
 $sql = "
 	SELECT TRXEqTermoReferencia,
@@ -111,6 +125,15 @@ if(isset($_POST['cmbUsuario'])){
 
 	<script src="global_assets/js/demo_pages/datatables_responsive.js"></script>
 	<script src="global_assets/js/demo_pages/datatables_sorting.js"></script>
+	<script src="global_assets/js/lamparinas/stop-back.js"></script>
+	
+	<script src="global_assets/js/demo_pages/form_layouts.js"></script>
+	<script src="global_assets/js/plugins/forms/styling/uniform.min.js"></script>	
+	
+	<!-- Validação -->
+	<script src="global_assets/js/plugins/forms/validation/validate.min.js"></script>
+	<script src="global_assets/js/plugins/forms/validation/localization/messages_pt_BR.js"></script>
+	<script src="global_assets/js/demo_pages/form_validation.js"></script>
 
 	<!-- /theme JS files -->	
 	
@@ -146,7 +169,7 @@ if(isset($_POST['cmbUsuario'])){
 				})
 			})
 
-
+             
 			$('#tblComissao').DataTable( {
 				"order": [[ 0, "asc" ]],
 			    autoWidth: false,
@@ -175,6 +198,39 @@ if(isset($_POST['cmbUsuario'])){
 					paginate: { 'first': 'Primeira', 'last': 'Última', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
 				}
 			});
+
+			$('#tblComissaoAnexo').DataTable( {
+				"order": [[ 0, "desc" ]],
+			    autoWidth: false,
+				responsive: true,
+			    columnDefs: [{ 
+					orderable: true,   //Data
+					width: "14%",
+					targets: [0]
+				},
+				{ 
+					orderable: true,   //Nome
+					width: "38%",
+					targets: [1]
+				},				
+				{ 
+					orderable: true,   //Arquivo
+					width: "38%",
+					targets: [2]
+				},
+				{ 
+					orderable: false,  //Ações
+					width: "10%",
+					targets: [3]
+				}],
+				dom: '<"datatable-header"fl><"datatable-scroll-wrap"t><"datatable-footer"ip>',
+				language: {
+					search: '<span>Filtro:</span> _INPUT_',
+					searchPlaceholder: 'filtra qualquer coluna...',
+					lengthMenu: '<span>Mostrar:</span> _MENU_',
+					paginate: { 'first': 'Primeira', 'last': 'Última', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
+				}
+			});
 			
 			// Select2 for length menu styling
 			var _componentSelect2 = function() {
@@ -193,8 +249,76 @@ if(isset($_POST['cmbUsuario'])){
 
 			_componentSelect2();
 			
-			/* Fim: Tabela Personalizada */		
+			/* Fim: Tabela Personalizada */	
+
+			//Valida Registro Duplicado
+			$('#enviar').on('click', function(e){
+				
+				e.preventDefault();
+
+				var inputDescricao = $('#inputNome').val();			
+				var inputFile = $('#inputArquivo').val();
+				var id = $("input:file").attr('id');
+				var tamanho =  1024 * 1024 * 32; //32MB
+
+				if (inputDescricao == ''){
+					$("#formAnexoComissao").submit();
+					$('#inputNome').focus();
+					return false;
+				}
+
+				//Verifica se o campo só possui espaços em branco
+				if (inputFile == ''){
+					alerta('Atenção','Selecione o arquivo!','error');
+					$("#formAnexoComissao").submit();
+					$('#inputArquivo').focus();
+					return false;
+				}
+								
+				//Verifica se a extensão é  diferente de PDF, DOC, DOCX, ODT, JPG, JPEG, PNG!
+				if (ext(inputFile) != 'pdf' && ext(inputFile) != 'doc' && ext(inputFile) != 'docx' && ext(inputFile) != 'odt' && ext(inputFile) != 'jpg' && ext(inputFile) != 'jpeg' && ext(inputFile) != 'png'){
+					alerta('Atenção','Por favor, envie arquivos com a seguinte extensão: PDF, DOC, DOCX, ODT, JPG, JPEG, PNG!','error');
+					$("#formAnexoComissao").submit();
+					$('#inputArquivo').focus();
+					return false;	
+				}
+				
+				//Verifica o tamanho do arquivo
+				if ($('#'+id)[0].files[0].size > tamanho){
+					alerta('Atenção','O arquivo enviado é muito grande, envie arquivos de até 32MB.','error');
+					$("#formAnexoComissao").submit();
+					$('#inputArquivo').focus();
+					return false;
+				}
+				
+				document.formAnexoComissao.action = 'trComissaoAnexoNovo.php';
+				document.formAnexoComissao.submit();
+			});
+
 		});
+
+		//Retorna a extenção do arquivo
+		function ext(path) {
+			var final = path.substr(path.lastIndexOf('/')+1);
+			var separador = final.lastIndexOf('.');
+			return separador <= 0 ? '' : final.substr(separador + 1);
+		}	
+
+		//Essa função foi criada para não usar $_GET e ficar mostrando os ids via URL
+		function removeComissaoAnexo(TRXCoId, TRXCoNome, TRXCoData, TRXCoArquivo, Tipo){
+
+			document.getElementById('inputComissaoAnexoID').value = TRXCoId;
+			document.getElementById('inputComissaoAnexoNome').value = TRXCoNome;
+			document.getElementById('inputComissaoAnexoData').value = TRXCoData;
+			document.getElementById('inputComissaoAnexoArquivo').value = TRXCoArquivo;	
+
+			if (Tipo == 'exclui'){
+					confirmaExclusao(document.formComissaoAnexoExclui, "Tem certeza que deseja excluir esse Anexo", "trComissaoAnexoExclui.php");
+			}
+
+			document.formComissaoAnexoExclui.action = 'trComissaoAnexoNovo.php';
+			document.formComissaoAnexoExclui.submit();
+		}	
 			
 		//Essa função foi criada para não usar $_GET e ficar mostrando os ids via URL
 		function atualizaComissao(TRXEqTermoReferencia, TRXEqUsuario, Tipo){
@@ -344,11 +468,115 @@ if(isset($_POST['cmbUsuario'])){
 					<!-- /info blocks -->			
 				</form>
 
+				<!-- Info blocks -->		
+				<div class="row">
+					<div class="col-lg-12">
+						<!-- Basic responsive configuration -->
+						<div class="card">
+							<div class="card-header header-elements-inline">
+								<h3 class="card-title">Anexos Comissão</h3>
+								<div class="header-elements">
+								</div>
+							</div>
+
+							<div class="card-body">							
+
+								<form name="formAnexoComissao" id="formAnexoComissao" method="post" enctype="multipart/form-data" class="form-validate-jquery">
+								 <input type="hidden" id="inputTRId" name="inputTRId" value="<?php echo $_SESSION['TRId']; ?>">
+									<div class="row">
+										<div class="col-lg-2">
+											<div class="form-group">
+												<label for="inputData">Data</label>
+												<input type="text" id="inputData" name="inputData" class="form-control" placeholder="Data" value="<?php echo date('d/m/Y'); ?>"  readOnly>
+											</div>
+										</div>
+										<div class="col-lg-10">
+											<div class="form-group">
+												<label for="inputNome">Descrição<span class="text-danger"> *</span></label>
+												<input type="text" id="inputNome" name="inputNome" class="form-control" placeholder="Descrição" required autofocus>
+											</div>
+										</div>
+									</div>	
+									<div class="row">
+										<div class="col-lg-12">
+											<label for="inputArquivo">Arquivo<span class="text-danger"> *</span></label>
+											<input type="file" id="inputArquivo" name="inputArquivo" class="form-control" required>
+										</div>
+									</div>	
+									<div class="row">	
+										<div class="col-lg-12">
+											<div class="form-group">										
+												Obs.: arquivos permitidos (.pdf, .doc, .docx, .odt, .jpg, .jpeg, .png) Tamanho máximo: 32MB
+											</div>
+										</div>									
+									</div>
+
+									<div class="row" style="margin-top: 10px;">
+										<div class="col-lg-12">								
+											<div class="form-group">
+												<button class="btn btn-lg btn-principal" id="enviar">Incluir</button>											
+											</div>
+										</div>
+									</div>
+
+								</form>
+							</div>
+
+							
+							<table class="table" id="tblComissaoAnexo">
+								<thead>
+									<tr class="bg-slate">
+										<th>Data</th>
+										<th>Descrição</th>
+										<th>Arquivo</th>										
+										<th class="text-center">Ações</th>
+									</tr>
+								</thead>
+								<tbody>
+
+									<?php foreach ($rowAnexo as $item){
+										print('
+											<tr>
+												<td>'.mostraData($item['TRXCoData']).'</td>
+
+												<td>'.$item['TRXCoNome'].'</td>
+
+												<td>
+													<a href="global_assets/anexos/comissao/'.$item['TRXCoArquivo'].'" target="_blank">'.$item['TRXCoArquivo'].'</a>
+												</td>
+												
+												<td class="text-center">
+													<div class="list-icons">
+														<div class="list-icons list-icons-extended">
+															<a href="#" onclick="removeComissaoAnexo('.$item['TRXCoId'].', \''.$item['TRXCoData'].'\',\''.$item['TRXCoNome'].'\', \''.$item['TRXCoArquivo'].'\', \'exclui\');" class="list-icons-item"><i class="icon-bin" data-popup="tooltip" data-placement="bottom" title="Exluir"></i></a>	
+														</div>
+													</div>
+												</td>
+
+											</tr>
+										');
+										}
+									?>
+
+								</tbody>
+							</table>
+						</div>
+						<!-- /basic responsive configuration -->
+					</div>
+				</div>	
+
 				<form name="formUpdatePresident" id='formUpdatePresident' method="post">
 					<input type="hidden" id="inputReferenceTermId" name="inputReferenceTermId">
 					<input type="hidden" id="inputUserId" name="inputUserId">
 					<input type="hidden" id="inputIsPresident" name="inputIsPresident">
 					<input type="hidden" id="inputUnitId" name="inputUnitId">
+				</form>
+
+				<form name="formComissaoAnexoExclui" method="post">
+					<input type="hidden" id="inputComissaoAnexoID" name="inputComissaoAnexoID">
+					<input type="hidden" id="inputComissaoAnexoData" name="inputComissaoAnexoData">
+					<input type="hidden" id="inputComissaoAnexoNome" name="inputComissaoAnexoNome">
+					<input type="hidden" id="inputComissaoAnexoArquivo" name="inputComissaoAnexoArquivo">
 				</form>
 
 			</div>
