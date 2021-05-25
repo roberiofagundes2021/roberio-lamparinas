8adff7321506c0524188fb8d96733189bdefa503
Carlos Victor <lorovictor@yahoo.com.br>
2021-05-25 16:16:02 -0300

    ProdutoOrcamento e ServicoOrcamento corrigidos. E pontuação nas quantidades das listas removido.

--- sistema/global_assets/js/lamparinas/custom.js
+++ sistema/global_assets/js/lamparinas/custom.js
@@ -131,7 +131,7 @@ function onlynumber(evt) {
 	var key = theEvent.keyCode || theEvent.which;
 	key = String.fromCharCode(key);
 	//var regex = /^[0-9.,]+$/;
-	var regex = /^[0-9.]+$/;
+	var regex = /^[0-9]+$/;
 	if (!regex.test(key)) {
 		theEvent.returnValue = false;
 		if (theEvent.preventDefault) theEvent.preventDefault();
