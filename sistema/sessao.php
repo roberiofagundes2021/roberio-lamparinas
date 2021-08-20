f665ee5f22abc1e8cc5f67616fbcf2674356b4fd
Paulo <marques.psg@gmail.com>
2021-08-20 13:14:50 -0300

    MOVIMENTAÇÃO FINANCEIRA:Editar: retirar o lancamentoId (GET) e passar a usar a edição via (POST) e Permissão edita e excluir.

--- sistema/sessao.php
+++ sistema/sessao.php
@@ -83,7 +83,7 @@ if (array_key_exists('AditivoNovo', $_SESSION) and !in_array(basename($_SERVER['
 $arquivosMovimentacaoFinanceira = array(
 	'movimentacaoFinanceira.php', 'movimentacaoFinanceiraFiltra.php', 'movimentacaoFinanceiraExclui.php', 'movimentacaoFinanceiraImprime.php', 'movimentacaoFinanceiraPagamento.php', 'movimentacaoFinanceiraRecebimento.php', 'movimentacaoFinanceiraTransferencia.php');
 
-if ((array_key_exists('MovFinancPeriodoDe', $_SESSION) || array_key_exists('MovFinancAte', $_SESSION) || array_key_exists('MovFinancContaBanco', $_SESSION) || array_key_exists('MovFinancPlanoContas', $_SESSION)|| array_key_exists('MovFinancCentroDeCustos', $_SESSION) || array_key_exists('MovFinancStatus', $_SESSION) || array_key_exists('MovFinancFormaPagamento', $_SESSION)) && !in_array(basename($_SERVER['PHP_SELF']), $arquivosMovimentacaoFinanceira)) {
+if ((array_key_exists('MovFinancPeriodoDe', $_SESSION) || array_key_exists('MovFinancAte', $_SESSION) || array_key_exists('MovFinancContaBanco', $_SESSION) || array_key_exists('MovFinancPlanoContas', $_SESSION)|| array_key_exists('MovFinancCentroDeCustos', $_SESSION) || array_key_exists('MovFinancStatus', $_SESSION) || array_key_exists('MovFinancFormaPagamento', $_SESSION) || array_key_exists('MovFinancPermissionAtualiza', $_SESSION)) && !in_array(basename($_SERVER['PHP_SELF']), $arquivosMovimentacaoFinanceira)) {
 	
 	unset($_SESSION['MovFinancPeriodoDe']);
 	unset($_SESSION['MovFinancAte']);
@@ -92,6 +92,7 @@ if ((array_key_exists('MovFinancPeriodoDe', $_SESSION) || array_key_exists('MovF
 	unset($_SESSION['MovFinancCentroDeCustos']);
 	unset($_SESSION['MovFinancStatus']);
 	unset($_SESSION['MovFinancFormaPagamento']);
+	unset($_SESSION['MovFinancPermissionAtualiza']);
 }
 
 $arquivosMovimentacaoFinanceiraConciliacao = array(
