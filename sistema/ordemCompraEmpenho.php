f6a6ee1453a52e423815bdff40833da4c7f47f85
Carlos Victor <lorovictor@yahoo.com.br>
2021-10-06 14:17:25 -0300

    Botão do Empenho grudado (resolvido)

--- sistema/ordemCompraEmpenho.php
+++ sistema/ordemCompraEmpenho.php
@@ -289,20 +289,17 @@
 
 												<div class="row" style="margin-top: 10px;">
 
-													<div class="col-lg-2">								
+													<div class="col-lg-12">								
 														<div class="form-group">
-																<button class="btn btn-lg btn-principal" id="enviar">Incluir</button>											
-														</div>
-													</div>
-													<div class="col-lg-10">								
-														<div class="form-group">');
+															<button class="btn btn-lg btn-principal" style="margin-right: 10px;" id="enviar">Incluir</button>');
+														
 															if ($count >= 1){
 																print('<a href="ordemCompraEmpenhoMudaSituacao.php" class="btn btn-lg btn-outline bg-slate-600 text-slate-600 border-slate">Finalizar Empenho</a>');													
 															}
-							     				 print('</div>
+																
+												print('	</div>
 													</div>
-												</div>
-										
+												</div>										
 
 											</form>
 										');	
