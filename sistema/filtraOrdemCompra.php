9ebe253161a1771ddcfe55fab6126c3fbc88f81b
Carlos Victor <lorovictor@yahoo.com.br>
2021-10-06 21:39:05 -0300

    Travando a edição do produto

--- sistema/filtraOrdemCompra.php
+++ sistema/filtraOrdemCompra.php
@@ -8,7 +8,7 @@ include('global_assets/php/conexao.php');
 $sql = " SELECT OrComId, OrComNumero
 		 FROM OrdemCompra
          JOIN Situacao on SituaId = OrComSituacao
-		 WHERE OrComUnidade = ". $_SESSION['UnidadeId'] . " and OrComFornecedor = " . $_GET['idFornecedor'] . " and SituaChave = 'LIBERADO' ";
+		 WHERE OrComUnidade = ". $_SESSION['UnidadeId'] . " and OrComFornecedor = " . $_GET['idFornecedor'] . " and SituaChave = 'LIBERADOCONTABILIDADE' ";
 $result = $conn->query($sql);
 $row = $result->fetchAll(PDO::FETCH_ASSOC);
 $count = count($row);
@@ -23,7 +23,7 @@ if ($count) {
                 FROM Movimentacao
                 JOIN Situacao on SituaId = MovimSituacao
                 WHERE MovimUnidade = ". $_SESSION['UnidadeId'] . " and MovimOrdemCompra = " . $value['OrComId'] . " 
-                and MovimTipo = 'E' and SituaChave = 'LIBERADO' ";
+                and MovimTipo = 'E' and SituaChave = 'LIBERADOCONTABILIDADE' ";
         $result = $conn->query($sql);
         $rowMovimentacao = $result->fetch(PDO::FETCH_ASSOC);
 
