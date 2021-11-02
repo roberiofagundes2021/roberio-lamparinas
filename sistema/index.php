ee86e06fe38864405d7c02e18ac313378cda5b0f
Carlos Victor <lorovictor@yahoo.com.br>
2021-11-02 18:39:11 -0300

    Ordem de Compra: Liberado para Liberado - Centro Administrativo

--- sistema/index.php
+++ sistema/index.php
@@ -449,7 +449,7 @@ if ($totalAcoes) {
 						document.formBandeja.setAttribute("target", "_self");
 						document.formBandeja.submit();								
 					} else if (Tipo == 'liberar') {
-						document.getElementById('inputOrdemCompraStatus').value = 'LIBERADO'; //Liberado
+						document.getElementById('inputOrdemCompraStatus').value = 'LIBERADOCENTRO'; //Liberado
 						document.formBandeja.action = "ordemcompraBandejaMudaSituacao.php";
 						document.formBandeja.setAttribute("target", "_self");
 						document.formBandeja.submit();
