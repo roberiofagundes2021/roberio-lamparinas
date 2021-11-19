79c0097fe727dfc9a04f068ebfc138db558f47fb
Carlos Victor <lorovictor@yahoo.com.br>
2021-11-19 12:14:09 -0300

    Validação de envio da movimentação para contabilidade sem anexo de NF

--- sistema/movimentacaoAnexoValida.php
+++ sistema/movimentacaoAnexoValida.php
@@ -6,7 +6,7 @@ include('global_assets/php/conexao.php');
 
 $sql = "SELECT COUNT(MvAneMovimentacao) Qtde
         FROM   MovimentacaoAnexo	
-        WHERE  MvAneUnidade = ". $_SESSION['UnidadeId'] ." AND MvAneMovimentacao = "$_POST['IMovim']." 
+        WHERE  MvAneUnidade = ". $_SESSION['UnidadeId'] ." AND MvAneMovimentacao = ".$_POST['IMovim']." 
         ";
         
 $result = $conn->query($sql);
@@ -15,7 +15,7 @@ $count = $row['Qtde'];
 
 //Verifica se já existe esse registro (se existir, retorna true )
 if($count){
-	echo 1;
+        echo 1;
 } else{
 	echo 0;
 }
