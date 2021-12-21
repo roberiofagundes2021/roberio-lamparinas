440b0a2b8cbb128f9209613c94bd00ad12dd75d6
Paulo <marques.psg@gmail.com>
2021-12-21 13:04:06 -0300

    FINANCEIRO / GRUPO DE CONTAS: caso de uso Grupo de Conta no  Apoio do módulo Financeiro

---
+++ sistema/grupoValida.php
@@ -0,0 +1,33 @@
+<?php 
+
+include_once("sessao.php"); 
+
+include('global_assets/php/conexao.php');
+
+if(isset($_POST['nomeVelho'])){
+	$sql = "SELECT GrConId
+			FROM GrupoConta
+			WHERE GrConUnidade = ".$_SESSION['UnidadeId']." and GrConNome = '". mssql_escape($_POST['nomeNovo'])."' and GrConNome <> '". mssql_escape($_POST['nomeVelho'])."'";
+} else{
+	$sql = "SELECT GrConId
+			FROM GrupoConta
+			WHERE GrConUnidade = ".$_SESSION['UnidadeId']." and GrConNome = '". mssql_escape($_POST['nomeNovo'])."'";
+}
+
+$result = $conn->query($sql);
+$row = $result->fetchAll(PDO::FETCH_ASSOC);
+$count = count($row);
+
+//Verifica se já existe esse registro (se existir, retorna true )
+if($count){
+	echo 1;
+} else{
+
+	if ($_POST['estadoAtual'] == 'EDITA'){
+		echo "EDITA";
+	} else{
+		echo 0;
+	}
+}
+
+?>
