cb073de2f3295536ef8f27ad027eeea7c37ca239
Paulo <marques.psg@gmail.com>
2022-06-07 19:16:40 -0300

    PROFISSIONAIS: criação do caso de uso anexos

---
+++ sistema/profissionalAnexo.php
@@ -0,0 +1,232 @@
+<?php 
+
+include_once("sessao.php"); 
+
+$_SESSION['PaginaAtual'] = 'Profissional Anexo';
+
+include('global_assets/php/conexao.php');
+
+if (isset($_POST['inputProfissionalId'])){
+	$_SESSION['idProfissional'] = $_POST['inputProfissionalId'];
+	$_SESSION['nomeProfissional'] = $_POST['inputProfissionalNome']; 
+}
+
+$sql = "SELECT PrAneId, PrAneData, PrAneNome, PrAneArquivo
+        FROM ProfissionalAnexo
+        WHERE PrAneUnidade = ". $_SESSION['UnidadeId'] ." and PrAneProfissional = ". $_SESSION['idProfissional'] ."
+        ORDER BY PrAneNome ASC";
+$result = $conn->query($sql);
+$row = $result->fetchAll(PDO::FETCH_ASSOC);
+//$count = count($row);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Lamparinas | Profissional Anexo</title>
+
+	<?php include_once("head.php"); ?>
+	
+	<!-- Theme JS files -->
+	<script src="global_assets/js/plugins/tables/datatables/datatables.min.js"></script>
+	<script src="global_assets/js/plugins/tables/datatables/extensions/responsive.min.js"></script>
+	
+	<script src="global_assets/js/plugins/forms/selects/select2.min.js"></script>
+	<script src="global_assets/js/demo_pages/form_select2.js"></script>
+	
+	<script src="global_assets/js/demo_pages/form_layouts.js"></script>
+	<script src="global_assets/js/plugins/forms/styling/uniform.min.js"></script>	
+
+	<script src="global_assets/js/demo_pages/datatables_responsive.js"></script>
+	<script src="global_assets/js/demo_pages/datatables_sorting.js"></script>
+	<script src="global_assets/js/lamparinas/stop-back.js"></script>
+	
+	<!-- /theme JS files -->	
+	
+	<script type="text/javascript">
+		
+		$(document).ready(function() {
+			
+			/* Início: Tabela Personalizada */
+			$('#tblProfissionalAnexo').DataTable( {
+				"order": [[ 0, "desc" ]],
+			    autoWidth: false,
+				responsive: true,
+			    columnDefs: [{ 
+					orderable: true,   //Data
+					width: "14%",
+					targets: [0]
+				},
+				{ 
+					orderable: true,   //Nome
+					width: "38%",
+					targets: [1]
+				},				
+				{ 
+					orderable: true,   //Arquivo
+					width: "38%",
+					targets: [2]
+				},
+				{ 
+					orderable: false,  //Ações
+					width: "10%",
+					targets: [3]
+				}],
+				dom: '<"datatable-header"fl><"datatable-scroll-wrap"t><"datatable-footer"ip>',
+				language: {
+					search: '<span>Filtro:</span> _INPUT_',
+					searchPlaceholder: 'filtra qualquer coluna...',
+					lengthMenu: '<span>Mostrar:</span> _MENU_',
+					paginate: { 'first': 'Primeira', 'last': 'Última', 'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;', 'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;' }
+				}
+			});
+			
+			// Select2 for length menu styling
+			var _componentSelect2 = function() {
+				if (!$().select2) {
+					console.warn('Warning - select2.min.js is not loaded.');
+					return;
+				}
+
+				// Initialize
+				$('.dataTables_length select').select2({
+					minimumResultsForSearch: Infinity,
+					dropdownAutoWidth: true,
+					width: 'auto'
+				});
+			};	
+
+			_componentSelect2();
+			
+			/* Fim: Tabela Personalizada */		
+		});
+			
+		//Essa função foi criada para não usar $_GET e ficar mostrando os ids via URL
+		function atualizaProfissionalAnexo(PrAneId, PrAneData, PrAneNome, PrAneArquivo, Tipo){
+
+				document.getElementById('inputProfissionalAnexoId').value = PrAneId;
+				document.getElementById('inputProfissionalAnexoNome').value = PrAneNome;
+				document.getElementById('inputProfissionalAnexoData').value = PrAneData;
+				document.getElementById('inputProfissionalAnexoArquivo').value = PrAneArquivo;	
+
+				if (Tipo == 'edita'){	
+					document.formProfissionalAnexo.action = "profissionalAnexoEdita.php";		
+				} else if (Tipo == 'exclui'){
+					confirmaExclusao(document.formProfissionalAnexo, "Tem certeza que deseja excluir esse Anexo", "profissionalAnexoExclui.php");
+			}
+			
+			document.formProfissionalAnexo.submit();
+		}		
+			
+	</script>
+
+</head>
+
+<body class="navbar-top">
+
+	<?php include_once("topo.php"); ?>	
+
+	<!-- Page content -->
+	<div class="page-content">
+		
+		<?php include_once("menu-left.php"); ?>
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<?php include_once("cabecalho.php"); ?>	
+
+			<!-- Content area -->
+			<div class="content">
+
+				<!-- Info blocks -->		
+				<div class="row">
+					<div class="col-lg-12">
+						<!-- Basic responsive configuration -->
+						<div class="card">
+							<div class="card-header header-elements-inline">
+								<h3 class="card-title">Relação de Anexos</h3>
+								<div class="header-elements">
+									<div class="list-icons">
+										<a class="list-icons-item" data-action="collapse"></a>
+										<a href="profissionalAnexo.php" class="list-icons-item" data-action="reload"></a>
+										<!--<a class="list-icons-item" data-action="remove"></a>-->
+									</div>
+								</div>
+							</div>
+
+							<div class="card-body">
+								A relação abaixo faz referência aos Anexos do profissional <span style="color: #FF0000; font-weight: bold;"> <?php echo $_SESSION['nomeProfissional']; ?> </span> da unidade <b><?php echo $_SESSION['UnidadeNome']; ?></b>
+								
+								<div class="text-right"><a href="profissional.php" role="button"><< Profissional</a>&nbsp;&nbsp;&nbsp;
+								<a href="profissionalAnexoNovo.php" class="btn btn-principal" role="button">Novo Anexo</a></div>
+							</div>
+							
+							<table class="table" id="tblProfissionalAnexo">
+								<thead>
+									<tr class="bg-slate">
+                                        <th>Data</th>
+										<th>Descrição</th>
+										<th>Arquivo</th>										
+										<th class="text-center">Ações</th>
+									</tr>
+								</thead>
+								<tbody>
+								<?php
+
+									foreach ($row as $item){
+										
+										print('
+										<tr>
+										    <td>'.mostraData($item['PrAneData']).'</td>
+                                            <td>'.$item['PrAneNome'].'</td>
+											<td><a href="global_assets/anexos/profissional/'.$item['PrAneArquivo'].'" target="_blank">'.$item['PrAneArquivo'].'</a></td>
+											');
+																										
+										print('<td class="text-center">
+												<div class="list-icons">
+													<div class="list-icons list-icons-extended">
+															<a href="#" onclick="atualizaProfissionalAnexo('.$item['PrAneId'].', \''.$item['PrAneData'].'\',\''.$item['PrAneNome'].'\', \''.$item['PrAneArquivo'].'\', \'edita\');" class="list-icons-item"><i class="icon-pencil7" data-popup="tooltip" data-placement="bottom" title="Editar"></i></a>
+															<a href="#" onclick="atualizaProfissionalAnexo('.$item['PrAneId'].', \''.$item['PrAneData'].'\',\''.$item['PrAneNome'].'\', \''.$item['PrAneArquivo'].'\', \'exclui\');" class="list-icons-item"><i class="icon-bin" data-popup="tooltip" data-placement="bottom" title="Exluir"></i></a>														
+													</div>
+												</div>
+											</td>
+										</tr>');
+									}
+								?>
+
+								</tbody>
+							</table>
+						</div>
+						<!-- /basic responsive configuration -->
+
+					</div>
+				</div>				
+				
+				<!-- /info blocks -->
+				
+				<form name="formProfissionalAnexo" method="post">
+					<input type="hidden" id="inputProfissionalAnexoId" name="inputProfissionalAnexoId">
+					<input type="hidden" id="inputProfissionalAnexoData" name="inputProfissionalAnexoData">
+					<input type="hidden" id="inputProfissionalAnexoNome" name="inputProfissionalAnexoNome">
+					<input type="hidden" id="inputProfissionalAnexoArquivo" name="inputProfissionalAnexoArquivo">
+				</form>
+
+			</div>
+			<!-- /content area -->
+			
+			<?php include_once("footer.php"); ?>
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+	<?php include_once("alerta.php"); ?>
+
+</body>
+
+</html>
---
+++ sistema/profissionalAnexoNovo.php
@@ -0,0 +1,224 @@
+<?php 
+
+include_once("sessao.php"); 
+
+$_SESSION['PaginaAtual'] = 'Novo Anexo';
+
+include('global_assets/php/conexao.php');
+
+if(isset($_POST['inputData'])){
+
+	try{	
+		
+		$_UP['pasta'] = 'global_assets/anexos/profissional/';
+
+		// Renomeia o arquivo? (Se true, o arquivo será salvo como .csv e um nome único)
+		$_UP['renomeia'] = false;
+
+		// Primeiro verifica se deve trocar o nome do arquivo
+		if ($_UP['renomeia'] == true) {
+		
+			// Cria um nome baseado no UNIX TIMESTAMP atual e com extensão .csv
+			//$nome_final = time().".".$extensao;
+			$nome_final = date('d-m-Y')."-".date('H-i-s')."-".$_FILES['inputArquivo']['name'];
+		
+		} else {
+		
+			// Mantém o nome original do arquivo
+			$nome_final = $_FILES['inputArquivo']['name'];
+		}
+		
+		//echo $_FILES['inputArquivo']['tmp_name']." <br>";
+		//echo $_UP['pasta'] . $nome_final." <br>";
+		
+		// Depois verifica se é possível mover o arquivo para a pasta escolhida
+		if (move_uploaded_file($_FILES['inputArquivo']['tmp_name'], $_UP['pasta'] . $nome_final)) {
+		
+			$sql = "INSERT INTO ProfissionalAnexo (PrAneData, PrAneNome, PrAneArquivo, PrAneProfissional, PrAneUsuarioAtualizador,PrAneUnidade)
+					VALUES (:iData, :sNome, :iArquivo, :iProfissional, :iUsuarioAtualizador, :iUnidade)";
+			$result = $conn->prepare($sql);
+					
+			$result->execute(array(
+							':iData' => gravaData($_POST['inputData']),
+							':sNome' => $_POST['inputNome'],
+							':iArquivo' => $nome_final,
+							':iProfissional' => $_SESSION['idProfissional'],
+							':iUsuarioAtualizador' => $_SESSION['UsuarId'],
+							':iUnidade' => $_SESSION['UnidadeId'],
+							));
+			
+			$_SESSION['msg']['titulo'] = "Sucesso";
+			$_SESSION['msg']['mensagem'] = "Anexo incluído!!!";
+			$_SESSION['msg']['tipo'] = "success";
+		}
+
+	} catch(PDOException $e) {
+		
+		$_SESSION['msg']['titulo'] = "Erro";
+		$_SESSION['msg']['mensagem'] = "Erro ao incluir Anexo!!!";
+		$_SESSION['msg']['tipo'] = "error";	
+		
+		echo 'Error: ' . $e->getMessage();
+	}
+	
+	irpara("profissionalAnexo.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Lamparinas | Anexo</title>
+
+	<?php include_once("head.php"); ?>
+	
+	<!-- Theme JS files -->
+	<script src="global_assets/js/plugins/forms/selects/select2.min.js"></script>
+	
+	<script src="global_assets/js/demo_pages/form_layouts.js"></script>
+	<script src="global_assets/js/plugins/forms/styling/uniform.min.js"></script>
+	<!-- /theme JS files -->
+
+	<!-- Validação -->
+	<script src="global_assets/js/plugins/forms/validation/validate.min.js"></script>
+	<script src="global_assets/js/plugins/forms/validation/localization/messages_pt_BR.js"></script>
+	<script src="global_assets/js/demo_pages/form_validation.js"></script>
+
+	<script type="text/javascript" >
+
+        $(document).ready(function() {
+			
+			//Valida Registro Duplicado
+			$('#enviar').on('click', function(e){
+				
+				e.preventDefault();
+							
+				var inputFile = $('#inputArquivo').val();
+				var id = $("input:file").attr('id');
+				var tamanho =  1024 * 1024 * 32; //32MB
+								
+				//Verifica se o campo só possui espaços em branco
+				if (inputFile == ''){
+					alerta('Atenção','Selecione o arquivo!','error');
+					$("#formProfissionalAnexo").submit();
+					$('#inputArquivo').focus();
+					return false;
+				}
+								
+				//Verifica se a extensão é  diferente de PDF, DOC, DOCX, ODT, JPG, JPEG, PNG!
+				if (ext(inputFile) != 'pdf' && ext(inputFile) != 'doc' && ext(inputFile) != 'docx' && ext(inputFile) != 'odt' && ext(inputFile) != 'jpg' && ext(inputFile) != 'jpeg' && ext(inputFile) != 'png'){
+					alerta('Atenção','Por favor, envie arquivos com a seguinte extensão: PDF, DOC, DOCX, ODT, JPG, JPEG, PNG!','error');
+					$("#formProfissionalAnexo").submit();
+					$('#inputArquivo').focus();
+					return false;	
+				}
+				
+				//Verifica o tamanho do arquivo
+				if ($('#'+id)[0].files[0].size > tamanho){
+					alerta('Atenção','O arquivo enviado é muito grande, envie arquivos de até 32MB.','error');
+					$("#formProfissionalAnexo").submit();
+					$('#inputArquivo').focus();
+					return false;
+				}				
+				
+					
+                $( "#formProfissionalAnexo" ).submit();
+				
+			})
+		})
+
+         //Retorna a extenção do arquivo
+		function ext(path) {
+			var final = path.substr(path.lastIndexOf('/')+1);
+			var separador = final.lastIndexOf('.');
+			return separador <= 0 ? '' : final.substr(separador + 1);
+		}	
+	</script>
+</head>
+
+<body class="navbar-top">
+
+	<?php include_once("topo.php"); ?>	
+
+	<!-- Page content -->
+	<div class="page-content">
+		
+		<?php include_once("menu-left.php"); ?>
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<?php include_once("cabecalho.php"); ?>	
+
+			<!-- Content area -->
+			<div class="content">
+				
+				<!-- Info blocks -->
+				<div class="card">
+					
+					<form name="formProfissionalAnexo" id="formProfissionalAnexo" method="post" enctype="multipart/form-data" class="form-validate-jquery">
+						<div class="card-header header-elements-inline">
+							<h5 class="text-uppercase font-weight-bold">Cadastrar Novo Anexo</h5>
+						</div>
+					
+					<div class="card-body">								
+							<div class="row">
+								<div class="col-lg-2">
+									<div class="form-group">
+										<label for="inputData">Data</label>
+										<input type="text" id="inputData" name="inputData" class="form-control" placeholder="Data" value="<?php echo date('d/m/Y'); ?>"  readOnly>
+									</div>
+								</div>
+								<div class="col-lg-10">
+									<div class="form-group">
+										<label for="inputNome">Descrição<span class="text-danger"> *</span></label>
+										<input type="text" id="inputNome" name="inputNome" class="form-control" placeholder="Descrição" required autofocus>
+									</div>
+								</div>
+							</div>	
+							<div class="row">
+								<div class="col-lg-12">
+									<label for="inputArquivo">Arquivo<span class="text-danger"> *</span></label>
+									<input type="file" id="inputArquivo" name="inputArquivo" class="form-control" required>
+								</div>
+							</div>	
+							<div class="row">	
+								<div class="col-lg-12">
+									<div class="form-group">										
+										Obs.: arquivos permitidos (.pdf, .doc, .docx, .odt, .jpg, .jpeg, .png) Tamanho máximo: 32MB
+									</div>
+								</div>									
+							</div>												
+							<div class="row" style="margin-top: 30px;">
+								<div class="col-lg-12">								
+									<div class="form-group">
+										<button class="btn btn-lg btn-principal" id="enviar">Incluir</button>
+										<a href="profissionalAnexo.php" class="btn btn-basic" role="button">Cancelar</a>
+									</div>
+								</div>
+							</div>
+						</form>								
+
+					</div>
+					<!-- /card-body -->
+					
+				</div>
+				<!-- /info blocks -->
+
+			</div>
+			<!-- /content area -->			
+			
+			<?php include_once("footer.php"); ?>
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+</body>
+</html>
