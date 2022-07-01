ae7472ad77fb13dd98f25b1c882e7ccba2a661f0
Paulo <marques.psg@gmail.com>
2022-07-01 16:22:52 -0300

    APOIO ATENDIMENTO / CAIXA: crud normal para os caixas. Apenas um campo NOME.

--- sistema/caixaValida.php
+++ sistema/caixaValida.php
@@ -4,32 +4,30 @@ include_once("sessao.php");
 
 include('global_assets/php/conexao.php');
 
-if(isset($_POST['nome'])){
+if(isset($_POST['nomeVelho'])){
 	$sql = "SELECT CaixaId
-		    FROM Caixa
-		    WHERE CaixaUnidade = ".$_SESSION['UnidadeId']." and CaixaNome = '".$_POST['nome']."'";
+			FROM Caixa
+			WHERE CaixaUnidade = ".$_SESSION['UnidadeId']." and CaixaNome = '". mssql_escape($_POST['nomeNovo'])."' and CaixaNome <> '". mssql_escape($_POST['nomeVelho'])."'";
+} else{
+	$sql = "SELECT CaixaId
+			FROM Caixa
+			WHERE CaixaUnidade = ".$_SESSION['UnidadeId']." and CaixaNome = '". mssql_escape($_POST['nomeNovo'])."'";
 }
-		   
+
 $result = $conn->query($sql);
 $row = $result->fetchAll(PDO::FETCH_ASSOC);
-
 $count = count($row);
 
 //Verifica se já existe esse registro (se existir, retorna true )
 if($count){
-	if(isset($_POST['caixaId'])){
-        foreach ($row as $Caixa) {
-	        if($Caixa['CaixaId'] == $_POST['caixaId']){
-		       echo 0;
-	        } else {
-               echo 1;
-	        }
-	    }
-	} else {
-		echo 1;
-	}
+	echo 1;
 } else{
-	echo 0;
+
+	if ($_POST['estadoAtual'] == 'EDITA'){
+		echo "EDITA";
+	} else{
+		echo 0;
+	}
 }
 
 ?>
