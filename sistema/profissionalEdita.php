e22bf3edaf728fae9e9b2dc024ab5020d018ab1a
Paulo <marques.psg@gmail.com>
2022-07-22 18:02:07 -0300

    profissional: inclusão do search nos selects

--- sistema/profissionalEdita.php
+++ sistema/profissionalEdita.php
@@ -562,7 +562,7 @@ if(isset($_POST['inputTipo'])){
                                                 <div class="row">								
                                                     <div class="col-lg-3">
                                                         <label for="cmbProfissao">Profissão</label>
-                                                        <select id="cmbProfissao" name="cmbProfissao" class="form-control form-control-select2">
+                                                        <select id="cmbProfissao" name="cmbProfissao" class="form-control select-search">
                                                             <option value="#">Selecione uma profissão</option>
                                                             <?php 
                                                                 $sql = "SELECT ProfiId, ProfiNome
@@ -584,7 +584,7 @@ if(isset($_POST['inputTipo'])){
 
 													<div class="col-lg-2">
                                                         <label for="cmbConselho">Conselho</label>
-                                                        <select id="cmbConselho" name="cmbConselho" class="form-control form-control-select2">
+                                                        <select id="cmbConselho" name="cmbConselho" class="form-control select-search">
                                                             <option value="#">Selecione </option>
 															 <?php 
                                                                 $sql = "SELECT PrConId, PrConNome
@@ -621,7 +621,7 @@ if(isset($_POST['inputTipo'])){
                                                 
                                                     <div class="col-lg-3">
                                                         <label for="cmbEspecialidade">Especialidades</label>
-                                                        <select id="cmbEspecialidade" name="cmbEspecialidade" class="form-control form-control-select2">
+                                                        <select id="cmbEspecialidade" name="cmbEspecialidade" class="form-control select-search">
                                                             <option value="#">Seleciona uma especialidade</option>
                                                             <?php
                                                             $sql = "SELECT EspecId, EspecNome
@@ -842,7 +842,7 @@ if(isset($_POST['inputTipo'])){
                                             <div class="row">								
                                                 <div class="col-lg-4">
                                                     <label for="cmbBanco">Banco</label>
-                                                    <select id="cmbBanco" name="cmbBanco" class="form-control form-control-select2">
+                                                    <select id="cmbBanco" name="cmbBanco" class="form-control select-search">
                                                         <option value="">Selecione</option>
                                                         <?php 
                                                             $sql = "SELECT CnBanId, CnBanNome
