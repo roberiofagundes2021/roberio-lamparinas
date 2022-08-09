bac02bbc849942c190ea03cc386c5b34874224ca
Carlos Victor <lorovictor@yahoo.com.br>
2022-08-08 23:22:02 -0300

    Histórico do Paciente, vários ajustes

--- sistema/atendimentoHistoricoValida.php
+++ sistema/atendimentoHistoricoValida.php
@@ -6,20 +6,34 @@ include('global_assets/php/conexao.php');
 
 if(isset($_POST['historicoId'])){
 
-	
 	$sql = "SELECT AtendNumRegistro, AtendDataRegistro, AtendId
 			FROM Atendimento
 			JOIN Cliente ON ClienId = AtendCliente
-			WHERE  AtendId = '". $_POST['historicoId']."'"; 
-} 
-$result = $conn->query($sql);
-$row = $result->fetchAll(PDO::FETCH_ASSOC);
-$count = count($row);
-
-//Verifica se já existe esse registro (se existir, retorna true )
-if($count){
-	echo 1;
-} else{
+			WHERE AtendId = '". $_POST['historicoId']."' and AtendUnidade = ".$_SESSION['UnidadeId']; 
+	$result = $conn->query($sql);
+	$row = $result->fetch(PDO::FETCH_ASSOC);
+	$count = count($row);	
+	
+	//Verifica se já existe esse registro (se existir, retorna true )
+	if($count){
+		print('
+			<p>Atend Id: '.$row['AtendId'].'</p>
+			<p>Número Registro: '.$row['AtendNumRegistro'].'</p>
+			<p>Data Registro: '.mostraData($row['AtendDataRegistro']).'</p>
+			<p>Atend Id: '.$row['AtendId'].'</p>
+			<p>Número Registro: '.$row['AtendNumRegistro'].'</p>
+			<p>Data Registro: '.mostraData($row['AtendDataRegistro']).'</p>
+			<p>Atend Id: '.$row['AtendId'].'</p>
+			<p>Número Registro: '.$row['AtendNumRegistro'].'</p>
+			<p>Data Registro: '.mostraData($row['AtendDataRegistro']).'</p>
+			<p>Atend Id: '.$row['AtendId'].'</p>
+			<p>Número Registro: '.$row['AtendNumRegistro'].'</p>
+			<p>Data Registro: '.mostraData($row['AtendDataRegistro']).'</p>
+		');
+	} else{
+		echo 0;
+	}
+} else {
 	echo 0;
 }
 
