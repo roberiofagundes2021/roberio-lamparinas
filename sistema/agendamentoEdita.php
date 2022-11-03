9adcd94472a4488535d473f5cf06837cfbe8cddd
samela <samelacbrito08@gmail.com>
2022-11-03 18:19:35 -0300

    LAMP-220 Código de serviço em agendamento edita

--- sistema/agendamentoEdita.php
+++ sistema/agendamentoEdita.php
@@ -568,7 +568,7 @@ $row = $result->fetch(PDO::FETCH_ASSOC);
 					$('#servico').empty();
 					$('#servico').append(`<option value=''>Selecione</option>`)
 					response.forEach(item => {
-						let opt = `<option value="${item.id}">${item.id} - ${item.nome}</option>`
+						let opt = `<option value="${item.id}">${item.codigo} - ${item.nome}</option>`
 						$('#servico').append(opt)
 					})
 				}
@@ -605,7 +605,7 @@ $row = $result->fetch(PDO::FETCH_ASSOC);
 					$('#servico').empty();
 					$('#servico').append(`<option value=''>Selecione</option>`)
 					response.forEach(item => {
-						let opt = `<option value="${item.id}">${item.id} - ${item.nome}</option>`
+						let opt = `<option value="${item.id}">${item.codigo} - ${item.nome}</option>`
 						$('#servico').append(opt)
 					})
 				}
