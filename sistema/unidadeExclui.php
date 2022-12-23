1d9d357b60b54b2337c10b3d583108748a95f33a
Djalma-Neto <djalma7743@gmail.com>
2022-12-23 15:55:37 -0300

    LAMP-301 criado passo a passo ao gerar nova unidade, falta ajustar a tela

--- sistema/unidadeExclui.php
+++ sistema/unidadeExclui.php
@@ -18,9 +18,9 @@ if(isset($_POST['inputUnidadeId'])){
 		$result = $conn->query($sql);
 		$result = $result->fetch(PDO::FETCH_ASSOC);
 
-		$sql = "DELETE FROM Perfil WHERE PerfiUnidade = $iUnidade";
-		$result = $conn->query($sql);
-		$result = $result->fetch(PDO::FETCH_ASSOC);
+		// $sql = "DELETE FROM Perfil WHERE PerfiUnidade = $iUnidade";
+		// $result = $conn->query($sql);
+		// $result = $result->fetch(PDO::FETCH_ASSOC);
 
 		$sql = "DELETE FROM LocalEstoque WHERE LcEstUnidade = $iUnidade";
 		$result = $conn->query($sql);
@@ -35,28 +35,48 @@ if(isset($_POST['inputUnidadeId'])){
 		$result = $result->fetch(PDO::FETCH_ASSOC);
 
 		$sql = "DELETE FROM AtendimentoClassificacao WHERE AtClaId = $iUnidade";
-		$result = $conn->prepare($sql);
+		$result = $conn->query($sql);
+		$result = $result->fetch(PDO::FETCH_ASSOC);
+
+		$sql = "DELETE FROM AtendimentoClassificacaoRisco WHERE AtClRUnidade = $iUnidade";
+		$result = $conn->query($sql);
+		$result = $result->fetch(PDO::FETCH_ASSOC);
+
+		$sql = "DELETE FROM CentroCusto WHERE CnCusUnidade = $iUnidade";
+		$result = $conn->query($sql);
+		$result = $result->fetch(PDO::FETCH_ASSOC);
+
+		$sql = "DELETE FROM AtendimentoModalidade WHERE AtModUnidade = $iUnidade";
+		$result = $conn->query($sql);
+		$result = $result->fetch(PDO::FETCH_ASSOC);
+
+		$sql = "DELETE FROM UsuarioXUnidade WHERE UsXUnUnidade = $iUnidade";
+		$result = $conn->query($sql);
+		$result = $result->fetch(PDO::FETCH_ASSOC);
+
+		$sql = "DELETE FROM Setor WHERE SetorUnidade = $iUnidade";
+		$result = $conn->query($sql);
 		$result = $result->fetch(PDO::FETCH_ASSOC);
 		
 		$sql = "DELETE FROM Unidade WHERE UnidaId = $iUnidade";
 		$result = $conn->query($sql);
-		$result = $result->fetch(PDO::FETCH_ASSOC); 
+		$result = $result->fetch(PDO::FETCH_ASSOC);
 		
 		$_SESSION['msg']['titulo'] = "Sucesso";
 		$_SESSION['msg']['mensagem'] = "Unidade excluída!!!";
-		$_SESSION['msg']['tipo'] = "success";		
+		$_SESSION['msg']['tipo'] = "success";
 		
 	} catch(PDOException $e) {
 		$conn->rollback();
 		
 		$_SESSION['msg']['titulo'] = "Erro";
 		$_SESSION['msg']['mensagem'] = "Não é possível excluir essa unidade, pois existem registros ligados a ela!!!";
-		$_SESSION['msg']['tipo'] = "error";			
+		$_SESSION['msg']['tipo'] = "error";
 		
-		//echo 'Error: ' . $e->getMessage();
+		var_dump($e);
 	}
 }
 
-irpara("unidade.php");
+// irpara("unidade.php");
 
 ?>
