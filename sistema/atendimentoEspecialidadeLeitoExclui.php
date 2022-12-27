626344e3fcd3b9589b3a95cd6688814004441835
samela <samelacbrito08@gmail.com>
2022-12-26 23:21:29 -0300

    LAMP-305 commit inicial

---
+++ sistema/atendimentoEspecialidadeLeitoExclui.php
@@ -0,0 +1,35 @@
+<?php 
+
+include_once("sessao.php");
+
+include('global_assets/php/conexao.php');
+
+if(isset($_POST['inputQuartoId'])){
+	
+	$iQuartoId = $_POST['inputQuartoId'];
+        	
+	try{
+		
+		$sql = "DELETE FROM Quarto
+				WHERE QuartId = :id";
+		$result = $conn->prepare($sql);
+		$result->bindParam(':id', $iQuartoId); 
+		$result->execute();
+		
+		$_SESSION['msg']['titulo'] = "Sucesso";
+		$_SESSION['msg']['mensagem'] = "Relação de Quarto excluída!!!";
+		$_SESSION['msg']['tipo'] = "success";		
+		
+	} catch(PDOException $e) {
+		
+		$_SESSION['msg']['titulo'] = "Erro";
+		$_SESSION['msg']['mensagem'] = "Erro ao excluir Relação de Quarto!!!";
+		$_SESSION['msg']['tipo'] = "error";			
+		
+		//echo 'Error: ' . $e->getMessage();die;
+	}
+}
+
+irpara("atendimentoRelacaoQuartos.php");
+
+?>
---
+++ sistema/atendimentoEspecialidadeLeitoValida.php
@@ -0,0 +1,26 @@
+<?php 
+
+include_once("sessao.php"); 
+
+include('global_assets/php/conexao.php');
+
+
+$sql = "SELECT QuartId
+        FROM Quarto
+        WHERE QuartUnidade = ".$_SESSION['UnidadeId']." and QuartNome = '". $_POST['nome']."' and QuartTipoInternacao = '". $_POST['tipoInternacao']."'";
+
+$result = $conn->query($sql);
+$row = $result->fetchAll(PDO::FETCH_ASSOC);
+$count = count($row);
+
+//Verifica se já existe esse registro (se existir, retorna true )
+if($count){
+	echo 1;
+} else{
+
+	if ($_POST['estadoAtual'] == 'EDITA'){
+		echo "EDITA";
+	} else{
+		echo 0;
+	}
+}
